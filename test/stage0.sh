175a82873defa7de25ffafd55825982c04e4fc09
wakirk <wakirk2002@gmail.com>
2025-09-18 10:53:05 -0500

    Update script name reference in stage0.sh

--- test/stage0.sh
+++ test/stage0.sh
@@ -5,7 +5,7 @@ source /root/lfs/lib/common-header.sh   # In every script.
 
 main () {  # alwasy have main.
 	echo "Hello World\n"
-	echo "I, $SCRIPT_NAME, am at $HERE supposed to be lfs-setup.sh"
+	echo "I, $SCRIPT_NAME, am at $HERE supposed to be stage0.sh"
 	bash --noprofile --norc -i
 	read
  }
