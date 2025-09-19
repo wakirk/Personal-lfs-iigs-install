fabde9a7fd9d420cb49c5386ce315d25626f7514
William Kirkpatrick <wakirk2002@gmail.com>
2025-09-18 23:45:50 -0500

    Working on getting menu to work #1

--- lfs-setup.sh
+++ lfs-setup.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
-source /root/lfs/lib/common-header.sh   # In every script.
+source /root/lfs/lib/common-header.lib   # In every script.
+source /root/lfs/lib/menu_system.lib
+
+
+test_func () {
+    echo "Test Function called with $HERE."
+	return 0
+}
 
 main () {  # alwasy have main.
-    echo "Hello World\n"
-	echo "I, $SCRIPT_NAME, am at $HERE supposed to be lfs-setup.sh"
- 	echo "running: $HERE/test/stage0.sh"
- 	read
+    menu_setup /root/lfs/mainmenu.tsv
+    echo "Hello World\n"3
 	read
- 	$HERE/test/stage0.sh
-	read
-	read
+	
+	menu_load
+	menu_run
+	
+	
+	menu_save
+	
+#	echo "I, $SCRIPT_NAME, am at $HERE supposed to be lfs-setup.sh"
+# 	echo "running: $HERE/test/stage0.sh"
+# 	read
+#	read
+# 	$HERE/test/stage0.sh
+#	read
+#	read
 	#	bash --noprofile --norc -i
  }
 
 
 
-source /root/lfs/lib/common-footer.sh   # in every script.
+source /root/lfs/lib/common-footer.lib   # in every script.
