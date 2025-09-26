8bf345023e25358bc7307c026693cd1f99fd44d4
William Kirkpatrick <wakirk2002@gmail.com>
2025-09-26 17:40:16 -0500

    Rebase after major failure 5

--- USB/LFS/lfs-pre.sh
+++ USB/LFS/lfs-pre.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+source userID.key
+
 clear
 mkdir /mnt/net/d -p
-mount -t cifs //192.168.1.237/D /mnt/net/d -o username=,password=,vers=3.0,uid=0,gid=0
+mount -t cifs $SHARE_ID $SHARE -o username=$SHARE_USER,password=$SHARE_PASS,vers=$SHARE_VERS,uid=0,gid=0
 lsblk
 ln -s /mnt/net/d/LFS /root/lfs
 cd /root/lfs
@@ -10,3 +12,4 @@ ls -l
 /root/lfs/bash/telnet-setup.sh
 grep -qxF 'export LFS=/mnt/lfs' /root/.bash_profile || echo 'export LFS=/mnt/lfs' >> /root/.bash_profile
 grep -qxF 'umask 022'           /root/.bash_profile || echo 'umask 022'           >> /root/.bash_profile
+
