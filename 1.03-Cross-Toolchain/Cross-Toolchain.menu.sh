be52092f7589a39168903988d9f01b5a3d46982d
William Kirkpatrick <wakirk2002@gmail.com>
2025-09-30 03:23:15 -0500

    Sync

---
+++ 1.03-Cross-Toolchain/Cross-Toolchain.menu.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+source /root/lfs/lib/menu.lib   # In every script.
+source /root/lfs/USB/userID.key  # Access Keys
+export $SHARE, $SHARE_USER, $SHARE_PASS, $SHARE_VERS, $SHARE_ID
+cd /root/lfs/1.02-Preparing
+
+Menu_Pre_Render() {
+	clear
+	echo " "
+	echo " "
+	echo "     Build System Software"
+	echo " "
+}
+
+main() {
+	menu_setup $HERE/Cross-Toolchain.menu.tsv
+	menu_load
+	check_auto
+	menu_run
+	menu_save
+}
+
+Menu_Post_Render() {
+	echoL "Main Menu"
+	echoR "System Software"
+	return 1
+}
+
+BinutilsPass1() {
+	mkdir -p $HERE/../Packages
+	echoL "Binutils-2.45 Pass 1"
+	lfs_share_on
+	run_as_lfs "$HERE/$EXEC_SCRIPT"
+	root_share_on
+	return_wait 1
+	return 1
+}
+
+GCCPass1() {
+	echoL "GCC-15.2.0 Pass 1"
+	lfs_share_on
+	run_as_lfs "$HERE/$EXEC_SCRIPT"
+	root_share_on
+	return_wait 1
+	return 1
+}
+
+LinuxHeaders() {
+	echoL "Linux-6.16.1 API Headers"
+	lfs_share_on
+	run_as_lfs "$HERE/$EXEC_SCRIPT"
+	root_share_on
+	return_wait 1
+	return 1
+}
+
+Glibc () {
+	echoL "Glibc-2.42"
+	lfs_share_on
+	run_as_lfs "$HERE/$EXEC_SCRIPT"
+	root_share_on
+	return_wait 1
+	return 1
+}
+
+Libstdc() {
+	echoL "Libstdc++ from GCC-15.2.0"
+	lfs_share_on
+	run_as_lfs "$HERE/$EXEC_SCRIPT"
+	root_share_on
+	return_wait 1
+	return 1
+}
+
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
+
+exit 0
--- lfs-setup.sh
+++ lfs-setup.sh
@@ -75,66 +75,51 @@ sys_preparing() {
 	return 1
 }
 
-sys_packages() {
-	echo "Packages and Patches"
-	"$HERE/1.03-Packages/$EXEC_SCRIPT"
-	echo "Packages and Patches Complete"
-	return 1
-}
-
-
-sys_preparations () {
-	echo "Final Preparations"
-	"$HERE/1.04-Preparations/$EXEC_SCRIPT"
-	echo "Final Preparations Complete"
-	return 1
-}
-
 lfs_Cross_Toolchain () {
 	echo "Compiling a Cross-Toolchain"
-	"$HERE/1.05-Cross-Toolchain/$EXEC_SCRIPT"
+	"$HERE/1.03-Cross-Toolchain/$EXEC_SCRIPT"
 	echo "Compiling a Cross-Toolchain Complete"
 	return 1
 }
 
 lfs_Cross_TempTools() {
 	echo "Cross Compiling Temporary Tools"
-	"$HERE/1.06-CrossComp-TempTools/$EXEC_SCRIPT"
+	"$HERE/1.04-CrossComp-TempTools/$EXEC_SCRIPT"
 	echo "Cross Compiling Temporary Tools Complete"
 	return 1
 }
 
 lfs_Cross_adddon_tempTools() {
 	echo "Entering Chroot and Building Additional Temporary Tools"
-	"$HERE/1.07-Chroot-Addon-TempTools/$EXEC_SCRIPT"
+	"$HERE/1.05-Chroot-Addon-TempTools/$EXEC_SCRIPT"
 	echo "Entering Chroot and Building Additional Temporary Tools Complete"
 	return 1
 }
 
 lfs_System_Software () {
 	echo "Installing Basic System Software"
-	"$HERE/1.08-Install-System-Software/$EXEC_SCRIPT"
+	"$HERE/1.06-Install-System-Software/$EXEC_SCRIPT"
 	echo "Installing Basic System Software Complete"
 	return 1
 }
 
 lfs_System_Config () {
 	echo "System Configuration"
-	"$HERE/1.09-System-Config/$EXEC_SCRIPT"
+	"$HERE/1.07-System-Config/$EXEC_SCRIPT"
 	echo "System Configuration Complete"
 	return 1
 }
 
 lfs_bootable () {
 	echo "Making the LFS System Bootable"
-	"$HERE/1.10-LFS-Bootable/$EXEC_SCRIPT"
+	"$HERE/1.18-LFS-Bootable/$EXEC_SCRIPT"
 	echo "Making the LFS System Bootable Complete"
 	return 1
 }
 
 lfs_Cleanup () {
 	echo "Cleanup and Final Steps"
-	"$HERE/1.11-Cleanup/$EXEC_SCRIPT"
+	"$HERE/1.09-Cleanup/$EXEC_SCRIPT"
 	echo "Cleanup and Final Steps Complete"
 	return 1
 }
