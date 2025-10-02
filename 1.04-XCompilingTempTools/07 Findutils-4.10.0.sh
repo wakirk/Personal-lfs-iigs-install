428cc29d13ba0a8c234e4bc43d700a8c7b33743e
wakirk <wakirk2002@gmail.com>
2025-10-02 03:27:13 -0500

    Refactor Findutils installation script

--- 1.04-XCompilingTempTools/07 Findutils-4.10.0.sh
+++ 1.04-XCompilingTempTools/07 Findutils-4.10.0.sh
@@ -1,36 +1,46 @@
-
-
-Findutils (4.10.0) - 2,189 KB:
-Home page: https://www.gnu.org/software/findutils/
-Download: https://ftp.gnu.org/gnu/findutils/findutils-4.10.0.tar.xz
-MD5 sum: 870cfd71c07d37ebe56f9f4aaf4ad872
-
-#
 #!/bin/bash
-#source /home/lfs/lfs/lib/menu.lib   # In every script.
-###
-#main () {
-#	clear
-#	echoR "Installing Software"
-#	echoL "Downloading ..."
-#	echo "Downloading... "
-#	../bash/Download.sh https*.xz *.xz
-#	cp ../Packages/*.xz $LFS/sources
-#	cd $LFS/sources
-#	ls
-#	echoL "Unpack: ..."
-#	sleep 2
-#	rm -fR *
-#	tar -vxsf *
-#	cd *
-#	echoL "Building ..."
-#	echoL "Installing ..."
-#	echoL "Install Complete "
-#	sleep 2
-#	cd ..
-#	rm -fR *
-#	exit 1
-#}
-#lfs_identity
-#lfs_tmux_entry main  # must be called after the routine it defines.
-#
+
+source /home/lfs/lfs/lib/menu.lib   # In every script.
+
+#Findutils (4.10.0) - 2,189 KB:
+#Home page: https://www.gnu.org/software/findutils/
+#Download: https://ftp.gnu.org/gnu/findutils/findutils-4.10.0.tar.xz
+#MD5 sum: 870cfd71c07d37ebe56f9f4aaf4ad872
+
+main () {
+  clear
+  echoR "Installing Software Findutils (4.10.0)"
+  echoL "Downloading Findutils (4.10.0)..."
+
+  echo "Downloading Findutils (4.10.0)... "
+  ../bash/Download.sh https://ftp.gnu.org/gnu/findutils/findutils-4.10.0.tar.xz findutils-4.10.0.tar.xz
+  cp ../Packages/findutils-4.10.0.tar.xz $LFS/sources
+  cd $LFS/sources
+  ls
+
+  echoL "Unpack: Findutils (4.10.0)..."
+  sleep 2
+  rm -fR findutils-4.10.0
+  tar -vxsf findutils-4.10.0.tar.xz
+  cd findutils-4.10.0
+
+  echoL "Building Findutils (4.10.0)..."
+  ./configure --prefix=/usr                 \
+            --localstatedir=/var/lib/locate \
+            --host=$LFS_TGT                 \
+            --build=$(build-aux/config.guess)
+  make
+
+  echoL "Installing Findutils (4.10.0)..."
+  make DESTDIR=$LFS install
+  
+  echoL "Install Complete Findutils (4.10.0)"
+  sleep 2
+  cd ..
+  rm -fR findutils-4.10.0
+
+  exit 1
+}
+
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
