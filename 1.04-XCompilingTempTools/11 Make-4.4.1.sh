e0980677e633fc0283a5253ad3995564959ca94d
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-02 10:42:38 -0500

    Sync

--- 1.04-XCompilingTempTools/11 Make-4.4.1.sh
+++ 1.04-XCompilingTempTools/11 Make-4.4.1.sh
@@ -12,36 +12,34 @@ main () {
 	echoR "Installing Software"
 	echoL "Downloading Make (4.4.1)..."
 
-            echo "Downloading Make (4.4.1)..."
+	echo "Downloading Make (4.4.1)..."
 	../bash/Download.sh https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz make-4.4.1.tar.gz
 	cp ../Packages/make-4.4.1.tar.gz $LFS/sources
 	cd $LFS/sources
 	ls
 
-            echoL "Unpack: Make (4.4.1)..."
+	echoL "Unpack: Make (4.4.1)..."
 	sleep 2
 	rm -fR make-4.4.1
 	tar -vxsf make-4.4.1.tar.gz
 	cd make-4.4.1
 
-            echoL "Building Make (4.4.1)..."
+	echoL "Building Make (4.4.1)..."
 	./configure --prefix=/usr   \
-            --host=$LFS_TGT \
-            --build=$(build-aux/config.guess)
+		--host=$LFS_TGT \
+		--build=$(build-aux/config.guess)
 	make
 
-            echoL "Installing Make (4.4.1)..."
+	echoL "Installing Make (4.4.1)..."
 	make DESTDIR=$LFS install
 
-            echoL "Install Complete Make (4.4.1)"
+	echoL "Install Complete Make (4.4.1)"
 	sleep 2
 	cd ..
 	rm -fR make-4.4.1
-            
+
 	exit 1
 }
 
 lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
-
-#
