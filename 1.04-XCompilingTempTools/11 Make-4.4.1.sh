4879097a420705cb456bb8d3cf8f583590d0dafa
wakirk <wakirk2002@gmail.com>
2025-10-02 10:40:29 -0500

    Update Make-4.4.1.sh for installation process
    Refactor script to include downloading, unpacking, and installing Make 4.4.1.

--- 1.04-XCompilingTempTools/11 Make-4.4.1.sh
+++ 1.04-XCompilingTempTools/11 Make-4.4.1.sh
@@ -1,40 +1,47 @@
-
-
-
-#
 #!/bin/bash
-#source /home/lfs/lfs/lib/menu.lib   # In every script.
-Make (4.4.1) - 2,300 KB:
-Home page: https://www.gnu.org/software/make/
-Download: https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz
-MD5 sum: c8469a3713cbbe04d955d4ae4be23eeb
-#main () {
-#	clear
-#	echoR "Installing Software"
-#	echoL "Downloading ..."
-#	echo "Downloading... "
-#	../bash/Download.sh https*.xz *.xz
-#	cp ../Packages/*.xz $LFS/sources
-#	cd $LFS/sources
-#	ls
-#	echoL "Unpack: ..."
-#	sleep 2
-#	rm -fR *
-#	tar -vxsf *
-#	cd *
-#	echoL "Building ..."
-./configure --prefix=/usr   \
+
+source /home/lfs/lfs/lib/menu.lib   # In every script.
+
+#Make (4.4.1) - 2,300 KB:
+#Home page: https://www.gnu.org/software/make/
+#Download: https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz
+#MD5 sum: c8469a3713cbbe04d955d4ae4be23eeb
+
+main () {
+	clear
+	echoR "Installing Software"
+	echoL "Downloading Make (4.4.1)..."
+
+            echo "Downloading Make (4.4.1)..."
+	../bash/Download.sh https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz make-4.4.1.tar.gz
+	cp ../Packages/make-4.4.1.tar.gz $LFS/sources
+	cd $LFS/sources
+	ls
+
+            echoL "Unpack: Make (4.4.1)..."
+	sleep 2
+	rm -fR make-4.4.1
+	tar -vxsf make-4.4.1.tar.gz
+	cd make-4.4.1
+
+            echoL "Building Make (4.4.1)..."
+	./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(build-aux/config.guess)
-make
-#	echoL "Installing ..."
-make DESTDIR=$LFS install
-#	echoL "Install Complete "
-#	sleep 2
-#	cd ..
-#	rm -fR *
-#	exit 1
-#}
-#lfs_identity
-#lfs_tmux_entry main  # must be called after the routine it defines.
+	make
+
+            echoL "Installing Make (4.4.1)..."
+	make DESTDIR=$LFS install
+
+            echoL "Install Complete Make (4.4.1)"
+	sleep 2
+	cd ..
+	rm -fR make-4.4.1
+            
+	exit 1
+}
+
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
+
 #
