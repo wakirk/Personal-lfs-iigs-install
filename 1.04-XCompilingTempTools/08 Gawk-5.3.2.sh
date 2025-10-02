8faa2d413d4ac63924743a1f282f0bd343b70987
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-02 04:20:38 -0500

    Sync

--- 1.04-XCompilingTempTools/08 Gawk-5.3.2.sh
+++ 1.04-XCompilingTempTools/08 Gawk-5.3.2.sh
@@ -8,38 +8,38 @@ source /home/lfs/lfs/lib/menu.lib   # In every script.
 #MD5 sum: b7014650c5f45e5d4837c31209dc0037
 
 main () {
-	clear	
-  echoR "Installing Software"
+	clear
+	echoR "Installing Software"
 	echoL "Downloading Gawk (5.3.2)..."
 
-  echo "Downloading Gawk (5.3.2)... "
-  ../bash/Download.sh https://ftp.gnu.org/gnu/gawk/gawk-5.3.2.tar.xz gawk-5.3.2.tar.xz
-  cp ../Packages/gawk-5.3.2.tar.xz $LFS/sources
-  cd $LFS/sources
-  ls
-  
+	echo "Downloading Gawk (5.3.2)... "
+	../bash/Download.sh https://ftp.gnu.org/gnu/gawk/gawk-5.3.2.tar.xz gawk-5.3.2.tar.xz
+	cp ../Packages/gawk-5.3.2.tar.xz $LFS/sources
+	cd $LFS/sources
+	ls
+
 	echoL "Unpack: Gawk (5.3.2)..."
-  sleep 2
-  rm -fR gawk-5.3.2
-  tar -vxsf gawk-5.3.2.tar.xz
-  cd gawk-5.3.2
+	sleep 2
+	rm -fR gawk-5.3.2
+	tar -vxsf gawk-5.3.2.tar.xz
+	cd gawk-5.3.2
 
-  echoL "Building Gawk (5.3.2)..."
-  sed -i 's/extras//' Makefile.in
-  ./configure --prefix=/usr   \
-            --host=$LFS_TGT \
-            --build=$(build-aux/config.guess)
-  make
-  
+	echoL "Building Gawk (5.3.2)..."
+	sed -i 's/extras//' Makefile.in
+	./configure --prefix=/usr   \
+		--host=$LFS_TGT \
+		--build=$(build-aux/config.guess)
+	make
+
 	echoL "Installing Gawk (5.3.2)..."
-  make DESTDIR=$LFS install
-  
-  echoL "Install Complete Gawk (5.3.2)"
-  sleep 2
-  cd ..
-  rm -fR gawk-5.3.2
+	make DESTDIR=$LFS install
 
-  exit 1
+	echoL "Install Complete Gawk (5.3.2)"
+	sleep 2
+	cd ..
+	rm -fR gawk-5.3.2
+
+	exit 1
 }
 
 lfs_identity
