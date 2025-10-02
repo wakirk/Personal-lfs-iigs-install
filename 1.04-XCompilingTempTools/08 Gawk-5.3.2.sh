13860b00bd0c69d832f3ac500618459ebc2cb824
wakirk <wakirk2002@gmail.com>
2025-10-02 04:18:58 -0500

    Refactor Gawk installation script for clarity
    Updated the Gawk installation script to include download and build steps.

--- 1.04-XCompilingTempTools/08 Gawk-5.3.2.sh
+++ 1.04-XCompilingTempTools/08 Gawk-5.3.2.sh
@@ -1,36 +1,46 @@
-
-
-Gawk (5.3.2) - 3,662 KB:
-Home page: https://www.gnu.org/software/gawk/
-Download: https://ftp.gnu.org/gnu/gawk/gawk-5.3.2.tar.xz
-MD5 sum: b7014650c5f45e5d4837c31209dc0037
-
-#
 #!/bin/bash
-#source /home/lfs/lfs/lib/menu.lib   # In every script.
-###
-#main () {
-#	clear
-#	echoR "Installing Software"
-#	echoL "Downloading ..."
-#	echo "Downloading... "
-#	../bash/Download.sh https*.xz *.xz
-#	cp ../Packages/*.xz $LFS/sources
-#	cd $LFS/sources
-#	ls
-#	echoL "Unpack: ..."
-#	sleep 2
-#	rm -fR *
-#	tar -vxsf *
-#	cd *
-#	echoL "Building ..."
-#	echoL "Installing ..."
-#	echoL "Install Complete "
-#	sleep 2
-#	cd ..
-#	rm -fR *
-#	exit 1
-#}
-#lfs_identity
-#lfs_tmux_entry main  # must be called after the routine it defines.
-#
+
+source /home/lfs/lfs/lib/menu.lib   # In every script.
+
+#Gawk (5.3.2) - 3,662 KB:
+#Home page: https://www.gnu.org/software/gawk/
+#Download: https://ftp.gnu.org/gnu/gawk/gawk-5.3.2.tar.xz
+#MD5 sum: b7014650c5f45e5d4837c31209dc0037
+
+main () {
+	clear	
+  echoR "Installing Software"
+	echoL "Downloading Gawk (5.3.2)..."
+
+  echo "Downloading Gawk (5.3.2)... "
+  ../bash/Download.sh https://ftp.gnu.org/gnu/gawk/gawk-5.3.2.tar.xz gawk-5.3.2.tar.xz
+  cp ../Packages/gawk-5.3.2.tar.xz $LFS/sources
+  cd $LFS/sources
+  ls
+  
+	echoL "Unpack: Gawk (5.3.2)..."
+  sleep 2
+  rm -fR gawk-5.3.2
+  tar -vxsf gawk-5.3.2.tar.xz
+  cd gawk-5.3.2
+
+  echoL "Building Gawk (5.3.2)..."
+  sed -i 's/extras//' Makefile.in
+  ./configure --prefix=/usr   \
+            --host=$LFS_TGT \
+            --build=$(build-aux/config.guess)
+  make
+  
+	echoL "Installing Gawk (5.3.2)..."
+  make DESTDIR=$LFS install
+  
+  echoL "Install Complete Gawk (5.3.2)"
+  sleep 2
+  cd ..
+  rm -fR gawk-5.3.2
+
+  exit 1
+}
+
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
