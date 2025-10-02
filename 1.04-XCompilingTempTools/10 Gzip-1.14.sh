201719396b04dd7ba9a7d0d5150f6d3493f24d00
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-02 04:51:42 -0500

    Sync

--- 1.04-XCompilingTempTools/10 Gzip-1.14.sh
+++ 1.04-XCompilingTempTools/10 Gzip-1.14.sh
@@ -8,31 +8,36 @@ source /home/lfs/lfs/lib/menu.lib   # In every script.
 #MD5 sum: 4bf5a10f287501ee8e8ebe00ef62b2c2
 
 main () {
-clear
-echoR "Installing Software"
-echoL "Downloading Gzip (1.14)..."
-echo "Downloading Gzip (1.14)... "
-../bash/Download.sh https://ftp.gnu.org/gnu/gzip/gzip-1.14.tar.xz gzip-1.14.tar.xz
-cp ../Packages/gzip-1.14.tar.xz $LFS/sources
-cd $LFS/sources
-ls
-echoL "Unpack: Gzip (1.14)..."
-sleep 2
-rm -fR gzip-1.14
-tar -vxsf gzip-1.14.tar.xz
-cd gzip-1.14
-echoL "Building Gzip (1.14)..."
-./configure --prefix=/usr --host=$LFS_TGT
-make
-echoL "Installing Gzip (1.14)..."
-make DESTDIR=$LFS install
-echoL "Install Complete Gzip (1.14)"
-sleep 2
-cd ..
-rm -fR gzip-1.14
-exit 1
+	clear
+	echoR "Installing Software"
+	echoL "Downloading Gzip (1.14)..."
+
+	echo "Downloading Gzip (1.14)... "
+	../bash/Download.sh https://ftp.gnu.org/gnu/gzip/gzip-1.14.tar.xz gzip-1.14.tar.xz
+	cp ../Packages/gzip-1.14.tar.xz $LFS/sources
+	cd $LFS/sources
+	ls
+
+	echoL "Unpack: Gzip (1.14)..."
+	sleep 2
+	rm -fR gzip-1.14
+	tar -vxsf gzip-1.14.tar.xz
+	cd gzip-1.14
+
+	echoL "Building Gzip (1.14)..."
+	./configure --prefix=/usr --host=$LFS_TGT
+	make
+
+	echoL "Installing Gzip (1.14)..."
+	make DESTDIR=$LFS install
+
+	echoL "Install Complete Gzip (1.14)"
+	sleep 2
+	cd ..
+	rm -fR gzip-1.14
+
+	exit 1
 }
 
 lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
-
