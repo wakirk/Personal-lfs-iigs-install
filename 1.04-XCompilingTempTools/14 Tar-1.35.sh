027c5df7bc33bdb0d2f25e87a52968f84e3266fd
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-03 07:27:17 -0500

    Sync

--- 1.04-XCompilingTempTools/14 Tar-1.35.sh
+++ 1.04-XCompilingTempTools/14 Tar-1.35.sh
@@ -10,7 +10,7 @@ source /home/lfs/lfs/lib/menu.lib   # In every script.
 main () {
 	clear
 	echoR "Installing Software Tar (1.35)"
-	echoL "Downloading ..."
+	echoL "Downloading Tar (1.35)..."
 
 	echo "Downloading Tar (1.35)... "
 	../bash/Download.sh https://ftp.gnu.org/gnu/tar/tar-1.35.tar.xz tar-1.35.tar.xz
@@ -18,27 +18,27 @@ main () {
 	cd $LFS/sources
 	ls
 
-            echoL "Unpack: Tar (1.35)..."
+	echoL "Unpack: Tar (1.35)..."
 	sleep 2
 	rm -fR tar-1.35
 	tar -vxsf tar-1.35.tar.xz
-	cd tar-1.35.tar.xz
+	cd tar-1.35
 
-            echoL "Building Tar (1.35)..."
+	echoL "Building Tar (1.35)..."
 	./configure --prefix=/usr   \
-            --host=$LFS_TGT \
-            --build=$(build-aux/config.guess)
+		--host=$LFS_TGT \
+		--build=$(build-aux/config.guess)
 	make
 
 	echoL "Installing Tar (1.35)..."
 	make DESTDIR=$LFS install
 
-            echoL "Install Complete Tar (1.35)"
+	echoL "Install Complete Tar (1.35)"
 	sleep 2
 	cd ..
 	rm -fR tar-1.35
 
-            exit 1
+	exit 1
 }
 
 lfs_identity
