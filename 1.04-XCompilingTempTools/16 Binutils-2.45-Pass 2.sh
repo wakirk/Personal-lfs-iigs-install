d0e247e56713cbf37ac97f54362a484ff84a580d
wakirk <wakirk2002@gmail.com>
2025-10-03 08:36:44 -0500

    Refactor Binutils installation script for Pass 2
    Updated the script for installing Binutils (2.45) Pass 2, including restructured comments and added echo statements for clarity.

--- 1.04-XCompilingTempTools/16 Binutils-2.45-Pass 2.sh
+++ 1.04-XCompilingTempTools/16 Binutils-2.45-Pass 2.sh
@@ -1,34 +1,31 @@
-Binutils (2.45) - 27,216 KB:
-Home page: https://www.gnu.org/software/binutils/
-Download: https://sourceware.org/pub/binutils/releases/binutils-2.45.tar.xz
-MD5 sum: dee5b4267e0305a99a3c9d6131f45759
-
-#
 #!/bin/bash
-#source /home/lfs/lfs/lib/menu.lib   # In every script.
-###
-#main () {
-#	clear
-#	echoR "Installing Software"
-#	echoL "Downloading ..."
-#	echo "Downloading... "
-#	../bash/Download.sh https*.xz *.xz
-#	cp ../Packages/*.xz $LFS/sources
-#	cd $LFS/sources
-#	ls
-#	echoL "Unpack: ..."
-#	sleep 2
-#	rm -fR *
-#	tar -vxsf *
-#	cd *
-#	echoL "Building ..."
 
-sed '6031s/$add_dir//' -i ltmain.sh
+source /home/lfs/lfs/lib/menu.lib   # In every script.
 
-mkdir -v build
-cd       build
+#Binutils (2.45) - 27,216 KB:
+#Home page: https://www.gnu.org/software/binutils/
+#Download: https://sourceware.org/pub/binutils/releases/binutils-2.45.tar.xz
+#MD5 sum: dee5b4267e0305a99a3c9d6131f45759
 
-../configure                   \
+main () {
+	clear
+	echoR "Installing Software Binutils (2.45) Pass 2"
+
+#   Already downloaded...
+	cd $LFS/sources
+	ls
+
+	echoL "Unpack: Binutils (2.45) Pass 2..."
+	sleep 2
+	rm -fR binutils-2.45      # fresh build for pass 2
+	tar -vxsf binutils-2.45.tar.xz
+	cd binutils-2.45
+
+	echoL "Building Binutils (2.45) Pass 2..."
+    sed '6031s/$add_dir//' -i ltmain.sh
+    mkdir -v build
+    cd       build
+    ../configure                   \
     --prefix=/usr              \
     --build=$(../config.guess) \
     --host=$LFS_TGT            \
@@ -39,20 +36,20 @@ cd       build
     --enable-64-bit-bfd        \
     --enable-new-dtags         \
     --enable-default-hash-style=gnu
-make
+    make
 
-#	echoL "Installing ..."
+	echoL "Installing Binutils (2.45) Pass 2..."
+    make DESTDIR=$LFS install
+    rm -v $LFS/usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes,sframe}.{a,la}
 
-make DESTDIR=$LFS install
-rm -v $LFS/usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes,sframe}.{a,la}
+	echoL "Install Complete Binutils (2.45) Pass 2"
+	sleep 2
+	cd ..
+    cd ..
+	rm -fR binutils-2.45
 
+	exit 1
+}
 
-#	echoL "Install Complete "
-#	sleep 2
-#	cd ..
-#	rm -fR *
-#	exit 1
-#}
-#lfs_identity
-#lfs_tmux_entry main  # must be called after the routine it defines.
-#
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
