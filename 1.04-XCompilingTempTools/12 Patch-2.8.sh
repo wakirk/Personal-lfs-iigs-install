75af2523227f89ce1acb50956c69f40b091771c5
wakirk <wakirk2002@gmail.com>
2025-10-03 01:12:44 -0500

    Update installation script for Patch 2.8

--- 1.04-XCompilingTempTools/12 Patch-2.8.sh
+++ 1.04-XCompilingTempTools/12 Patch-2.8.sh
@@ -7,33 +7,39 @@ source /home/lfs/lfs/lib/menu.lib   # In every script.
 #Download: https://ftp.gnu.org/gnu/patch/patch-2.8.tar.xz
 #MD5 sum: 149327a021d41c8f88d034eab41c039f
 
-#main () {
-#	clear
-#	echoR "Installing Software"
-#	echoL "Downloading ..."
-#	echo "Downloading... "
-#	../bash/Download.sh https*.xz *.xz
-#	cp ../Packages/*.xz $LFS/sources
-#	cd $LFS/sources
-#	ls
-#	echoL "Unpack: ..."
-#	sleep 2
-#	rm -fR *
-#	tar -vxsf *
-#	cd *
-#	echoL "Building ..."
-./configure --prefix=/usr   \
+main () {
+	clear
+	echoR "Installing Software"
+	echoL "Downloading Patch (2.8)..."
+
+            echo "Downloading Patch (2.8)... "
+	../bash/Download.sh https://ftp.gnu.org/gnu/patch/patch-2.8.tar.xz patch-2.8.tar.xz
+	cp ../Packages/patch-2.8.tar.xz $LFS/sources
+	cd $LFS/sources
+	ls
+            
+	echoL "Unpack: Patch (2.8)..."
+	sleep 2
+	rm -fR patch-2.8
+	tar -vxsf patch-2.8.tar.xz
+	cd patch-2.8
+            
+	echoL "Building Patch (2.8)..."
+	./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(build-aux/config.guess)
-make
-#	echoL "Installing ..."
-make DESTDIR=$LFS install
-#	echoL "Install Complete "
-#	sleep 2
-#	cd ..
-#	rm -fR *
-#	exit 1
-#}
-#lfs_identity
-#lfs_tmux_entry main  # must be called after the routine it defines.
-#
+	make
+            
+	echoL "Installing Patch (2.8)..."
+	make DESTDIR=$LFS install
+            
+	echoL "Install Complete Patch (2.8)"
+	sleep 2
+	cd ..
+	rm -fR patch-2.8
+
+            exit 1
+}
+
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
