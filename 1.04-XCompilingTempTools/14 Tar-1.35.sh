77327eb3e9033e700c7f4cb03b7b8adb02762b77
wakirk <wakirk2002@gmail.com>
2025-10-03 07:06:16 -0500

    Refactor installation script for Tar 1.35
    Updated the script to install Tar 1.35 with proper download and build steps.

--- 1.04-XCompilingTempTools/14 Tar-1.35.sh
+++ 1.04-XCompilingTempTools/14 Tar-1.35.sh
@@ -1,41 +1,45 @@
-
-Tar (1.35) - 2,263 KB:
-Home page: https://www.gnu.org/software/tar/
-Download: https://ftp.gnu.org/gnu/tar/tar-1.35.tar.xz
-MD5 sum: a2d8042658cfd8ea939e6d911eaf4152
-
-
-#
 #!/bin/bash
-#source /home/lfs/lfs/lib/menu.lib   # In every script.
-###
-#main () {
-#	clear
-#	echoR "Installing Software"
-#	echoL "Downloading ..."
-#	echo "Downloading... "
-#	../bash/Download.sh https*.xz *.xz
-#	cp ../Packages/*.xz $LFS/sources
-#	cd $LFS/sources
-#	ls
-#	echoL "Unpack: ..."
-#	sleep 2
-#	rm -fR *
-#	tar -vxsf *
-#	cd *
-#	echoL "Building ..."
-./configure --prefix=/usr   \
+
+source /home/lfs/lfs/lib/menu.lib   # In every script.
+
+#Tar (1.35) - 2,263 KB:
+#Home page: https://www.gnu.org/software/tar/
+#Download: https://ftp.gnu.org/gnu/tar/tar-1.35.tar.xz
+#MD5 sum: a2d8042658cfd8ea939e6d911eaf4152
+
+main () {
+	clear
+	echoR "Installing Software Tar (1.35)"
+	echoL "Downloading ..."
+
+	echo "Downloading Tar (1.35)... "
+	../bash/Download.sh https://ftp.gnu.org/gnu/tar/tar-1.35.tar.xz tar-1.35.tar.xz
+	cp ../Packages/tar-1.35.tar.xz $LFS/sources
+	cd $LFS/sources
+	ls
+
+            echoL "Unpack: Tar (1.35)..."
+	sleep 2
+	rm -fR tar-1.35
+	tar -vxsf tar-1.35.tar.xz
+	cd tar-1.35.tar.xz
+
+            echoL "Building Tar (1.35)..."
+	./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(build-aux/config.guess)
-make
-#	echoL "Installing ..."
-make DESTDIR=$LFS install
-#	echoL "Install Complete "
-#	sleep 2
-#	cd ..
-#	rm -fR *
-#	exit 1
-#}
-#lfs_identity
-#lfs_tmux_entry main  # must be called after the routine it defines.
-#
+	make
+
+	echoL "Installing Tar (1.35)..."
+	make DESTDIR=$LFS install
+
+            echoL "Install Complete Tar (1.35)"
+	sleep 2
+	cd ..
+	rm -fR tar-1.35
+
+            exit 1
+}
+
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
