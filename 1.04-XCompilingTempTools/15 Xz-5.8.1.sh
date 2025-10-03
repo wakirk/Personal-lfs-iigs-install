64658fb00792d33d794216aa0b70e31ad283bc4f
wakirk <wakirk2002@gmail.com>
2025-10-03 08:25:07 -0500

    Update Xz Utils installation script for version 5.8.1

--- 1.04-XCompilingTempTools/15 Xz-5.8.1.sh
+++ 1.04-XCompilingTempTools/15 Xz-5.8.1.sh
@@ -1,44 +1,48 @@
-
-
-Xz Utils (5.8.1) - 1,428 KB:
-Home page: https://tukaani.org/xz
-Download: https://github.com//tukaani-project/xz/releases/download/v5.8.1/xz-5.8.1.tar.xz
-MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
-
-#
 #!/bin/bash
-#source /home/lfs/lfs/lib/menu.lib   # In every script.
-###
-#main () {
-#	clear
-#	echoR "Installing Software"
-#	echoL "Downloading ..."
-#	echo "Downloading... "
-#	../bash/Download.sh https*.xz *.xz
-#	cp ../Packages/*.xz $LFS/sources
-#	cd $LFS/sources
-#	ls
-#	echoL "Unpack: ..."
-#	sleep 2
-#	rm -fR *
-#	tar -vxsf *
-#	cd *
-#	echoL "Building ..."
-./configure --prefix=/usr                     \
+
+source /home/lfs/lfs/lib/menu.lib   # In every script.
+
+#Xz Utils (5.8.1) - 1,428 KB:
+#Home page: https://tukaani.org/xz
+#Download: https://github.com//tukaani-project/xz/releases/download/v5.8.1/xz-5.8.1.tar.xz
+#MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+
+main () {
+	clear
+	echoR "Installing Software Xz Utils (5.8.1)"
+	echoL "Downloading Xz Utils (5.8.1)..."
+
+	echo "Downloading Xz Utils (5.8.1)..."
+	../bash/Download.sh https://github.com//tukaani-project/xz/releases/download/v5.8.1/xz-5.8.1.tar.xz xz-5.8.1.tar.xz
+	cp ../Packages/xz-5.8.1.tar.xz $LFS/sources
+	cd $LFS/sources
+	ls
+
+	echoL "Unpack: Xz Utils (5.8.1)..."
+	sleep 2
+	rm -fR xz-5.8.1
+	tar -vxsf xz-5.8.1.tar.xz
+	cd xz-5.8.1
+
+	echoL "Building Xz Utils (5.8.1)..."
+            ./configure --prefix=/usr                     \
             --host=$LFS_TGT                   \
             --build=$(build-aux/config.guess) \
             --disable-static                  \
             --docdir=/usr/share/doc/xz-5.8.1
-make
-#	echoL "Installing ..."
-make DESTDIR=$LFS install
-rm -v $LFS/usr/lib/liblzma.la
-#	echoL "Install Complete "
-#	sleep 2
-#	cd ..
-#	rm -fR *
-#	exit 1
-#}
-#lfs_identity
-#lfs_tmux_entry main  # must be called after the routine it defines.
-#
+	make
+
+            echoL "Installing Xz Utils (5.8.1)..."
+	make DESTDIR=$LFS install
+	rm -v $LFS/usr/lib/liblzma.la
+
+            echoL "Install Complete Xz Utils (5.8.1)"
+	sleep 2
+	cd ..
+	rm -fR xz-5.8.1
+
+            exit 1
+}
+
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
