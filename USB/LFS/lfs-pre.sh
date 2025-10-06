96033ae9206812a04e0f7884e019ba9ecdf1fcb9
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-05 22:47:44 -0500

    Sync

--- USB/LFS/lfs-pre.sh
+++ USB/LFS/lfs-pre.sh
@@ -3,7 +3,7 @@
 source userID.key
 
 clear
-setfont /usr/share/kdb/latarcyrheb-sun32.psfx.gz
+setfont /usr/share/kbd/consolefonts/latarcyrheb-sun32.psfu.gz
 mkdir /mnt/net/d -p
 mount -t cifs $SHARE_ID $SHARE -o username=$SHARE_USER,password=$SHARE_PASS,vers=$SHARE_VERS,uid=0,gid=0
 lsblk
@@ -13,4 +13,6 @@ ls -l
 /root/lfs/bash/telnet-setup.sh
 grep -qxF 'export LFS=/mnt/lfs' /root/.bash_profile || echo 'export LFS=/mnt/lfs' >> /root/.bash_profile
 grep -qxF 'umask 022'           /root/.bash_profile || echo 'umask 022'           >> /root/.bash_profile
+
+#!/bin/bash
 
