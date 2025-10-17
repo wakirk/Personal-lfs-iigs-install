a7f35181bb92a9cc72469b19ca4ef72704932e5a
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-16 22:42:39 -0500

    Sync

--- 1.06-Install-System-Software/26 shadow-4.18.0.sh
+++ 1.06-Install-System-Software/26 shadow-4.18.0.sh
@@ -4,10 +4,10 @@ source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-Shadow (4.18.0) - 2,293 KB:
-Home page: https://github.com/shadow-maint/shadow/
-Download: https://github.com/shadow-maint/shadow/releases/download/4.18.0/shadow-4.18.0.tar.xz
-MD5 sum: 30ef46f54363db1d624587be68794ef2
+	# Shadow (4.18.0) - 2,293 KB:
+	# Home page: https://github.com/shadow-maint/shadow/
+	# Download: https://github.com/shadow-maint/shadow/releases/download/4.18.0/shadow-4.18.0.tar.xz
+	# MD5 sum: 30ef46f54363db1d624587be68794ef2
 	echoR "System Software"
 
 	echoL "Downloading Shadow (4.18.0)..."
@@ -25,32 +25,30 @@ MD5 sum: 30ef46f54363db1d624587be68794ef2
 
 	echoL "Building Shadow (4.18.0)..."
 	sleep 2
-sed -i 's/groups$(EXEEXT) //' src/Makefile.in
-find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \;
-find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \;
-find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \;
-sed -e 's:#ENCRYPT_METHOD DES:ENCRYPT_METHOD YESCRYPT:' \
-    -e 's:/var/spool/mail:/var/mail:'                   \
-    -e '/PATH=/{s@/sbin:@@;s@/bin:@@}'                  \
-    -i etc/login.defs
-touch /usr/bin/passwd
-./configure --sysconfdir=/etc   \
-            --disable-static    \
-            --with-{b,yes}crypt \
-            --without-libbsd    \
-            --with-group-name-max-length=32    
-make
-
+	sed -i 's/groups$(EXEEXT) //' src/Makefile.in
+	find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \;
+	find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \;
+	find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \;
+	sed -e 's:#ENCRYPT_METHOD DES:ENCRYPT_METHOD YESCRYPT:' \
+		-e 's:/var/spool/mail:/var/mail:'                   \
+		-e '/PATH=/{s@/sbin:@@;s@/bin:@@}'                  \
+		-i etc/login.defs
+	touch /usr/bin/passwd
+	./configure --sysconfdir=/etc \
+		--disable-static          \
+		--with-{b,yes}crypt       \
+		--without-libbsd          \
+		--with-group-name-max-length=32
+	make
 
 	echoL "Installing Shadow (4.18.0)..."
 	sleep 2
-make exec_prefix=/usr install
-make -C man install-man
-mkdir -p /etc/default
-  # useradd -D --gid 999
-  /bin/bash
- # passwd root password
-  
+	make exec_prefix=/usr install
+	make -C man install-man
+	mkdir -p /etc/default
+	useradd -D --gid 999
+	echo "password" | passwd root -s
+
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
@@ -63,18 +61,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
