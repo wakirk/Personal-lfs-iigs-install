c43dae2e4ccef9497b53460894d4a0c02ffd45b6
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-16 22:32:11 -0500

    Sync

--- 1.06-Install-System-Software/25 libxcrypt-4.4.38.sh
+++ 1.06-Install-System-Software/25 libxcrypt-4.4.38.sh
@@ -4,10 +4,10 @@ source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-Libxcrypt (4.4.38) - 612 KB:
-Home page: https://github.com/besser82/libxcrypt/
-Download: https://github.com/besser82/libxcrypt/releases/download/v4.4.38/libxcrypt-4.4.38.tar.xz
-MD5 sum: 1796a5d20098e9dd9e3f576803c83000
+	# Libxcrypt (4.4.38) - 612 KB:
+	# Home page: https://github.com/besser82/libxcrypt/
+	# Download: https://github.com/besser82/libxcrypt/releases/download/v4.4.38/libxcrypt-4.4.38.tar.xz
+	# MD5 sum: 1796a5d20098e9dd9e3f576803c83000
 	echoR "System Software"
 
 	echoL "Downloading Libxcrypt (4.4.38)..."
@@ -25,21 +25,21 @@ MD5 sum: 1796a5d20098e9dd9e3f576803c83000
 
 	echoL "Building Libxcrypt (4.4.38)..."
 	sleep 2
-./configure --prefix=/usr                \
-            --enable-hashes=strong,glibc \
-            --enable-obsolete-api=no     \
-            --disable-static             \
-            --disable-failure-tokens
-  make
-  
+	./configure --prefix=/usr        \
+		--enable-hashes=strong,glibc \
+		--enable-obsolete-api=no     \
+		--disable-static             \
+		--disable-failure-tokens
+	make
+
 	echoL "Testing Libxcrypt (4.4.38)..."
-  sleep 2
-  make check
-	
+	sleep 2
+	make check
+
 	echoL "Installing Libxcrypt (4.4.38)..."
 	sleep 2
-  make install
-	
+	make install
+
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
@@ -52,13 +52,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-
-
-
-
