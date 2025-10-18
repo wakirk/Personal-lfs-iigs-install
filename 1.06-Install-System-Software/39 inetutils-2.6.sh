a28431f66d4d7a8a3b7af4bc20ec2368908e77db
wakirk <wakirk2002@gmail.com>
2025-10-18 11:29:48 -0500

    Update inetutils installation script

---
+++ 1.06-Install-System-Software/39 inetutils-2.6.sh
@@ -0,0 +1,80 @@
+Inetutils (2.6) - 1,724 KB:
+Home page: https://www.gnu.org/software/inetutils/
+Download: https://ftp.gnu.org/gnu/inetutils/inetutils-2.6.tar.xz
+MD5 sum: 401d7d07682a193960bcdecafd03de94
+
+
+/bin/bash
+
+source /root/lfs/lib/menu.lib   # In every script.
+
+main () {
+
+	Utils (5.8.1) - 1,428 KB:
+	Home page: https://tukaani.org/xz
+	Download: https: .tar.xz
+	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	echoR "System Software"
+
+	echoL "Downloading ------- ( ) ..."
+	sleep 2
+	cd "/root/lfs/1.06-Install-System-Software"
+	../bash/Download.sh https://*.tar.xz *.tar.xz
+	cp ../Packages/-.tar.xz /sources
+
+	echoL "Unpack ------- ( ) ..."
+	sleep 2
+	cd /sources
+	rm -fR
+	tar -vxsf
+	cd 
+
+	echoL "Building ------- ( ) ..."
+	sleep 2
+
+	echoL "Testing ------- ( ) ..."
+	sleep 2
+	/bin/bash
+
+	echoL "Installing ------- ( ) ..."
+	sleep 2
+
+	echoL "Cleaning up build area...."
+	sleep 2
+	cd /sources
+	rm -fR 
+
+	echoL "Exiting..."
+}
+
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
+
+exit 1
+
+
+
+
+
+
+sed -i 's/def HAVE_TERMCAP_TGETENT/ 1/' telnet/telnet.c
+./configure --prefix=/usr        \
+            --bindir=/usr/bin    \
+            --localstatedir=/var \
+            --disable-logger     \
+            --disable-whois      \
+            --disable-rcp        \
+            --disable-rexec      \
+            --disable-rlogin     \
+            --disable-rsh        \
+            --disable-servers
+
+make
+make check
+make install
+mv -v /usr/{,s}bin/ifconfig
+
+
+
+
+
