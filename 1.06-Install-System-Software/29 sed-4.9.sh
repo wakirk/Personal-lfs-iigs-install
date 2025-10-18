3c14dc47c16c393273dc08a212f6420f2d99c6e1
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-17 22:46:20 -0500

    Sync

--- 1.06-Install-System-Software/29 sed-4.9.sh
+++ 1.06-Install-System-Software/29 sed-4.9.sh
@@ -4,10 +4,10 @@ source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-  Sed (4.9) - 1,365 KB:
-  Home page: https://www.gnu.org/software/sed/
-  Download: https://ftp.gnu.org/gnu/sed/sed-4.9.tar.xz
-  MD5 sum: 6aac9b2dbafcd5b7a67a8a9bcb8036c3
+	# Sed (4.9) - 1,365 KB:
+	# Home page: https://www.gnu.org/software/sed/
+	# Download: https://ftp.gnu.org/gnu/sed/sed-4.9.tar.xz
+	# MD5 sum: 6aac9b2dbafcd5b7a67a8a9bcb8036c3
 	echoR "System Software"
 
 	echoL "Downloading Sed (4.9)..."
@@ -25,20 +25,19 @@ main () {
 
 	echoL "Building Sed (4.9)..."
 	sleep 2
-  ./configure --prefix=/usr
-  make 
-  make html
+	./configure --prefix=/usr
+	make 
+	make html
 
 	echoL "Testing Sed (4.9)..."
-  chown -R tester .
-  su tester -c "PATH=$PATH make check"
-  /bin/bash
+	chown -R tester .
+	su tester -c "PATH=$PATH make check"
 
 	echoL "Installing Sed (4.9)..."
 	sleep 2
-  make install
-  install -d -m755           /usr/share/doc/sed-4.9
-  install -m644 doc/sed.html /usr/share/doc/sed-4.9
+	make install
+	install -d -m755           /usr/share/doc/sed-4.9
+	install -m644 doc/sed.html /usr/share/doc/sed-4.9
 
 	echoL "Cleaning up build area...."
 	sleep 2
@@ -52,4 +51,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
