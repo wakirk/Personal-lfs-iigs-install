ec2e1f2a04cd730305c17d26cbee16300a7c3e4e
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-18 17:49:00 -0500

    Sync

--- 1.06-Install-System-Software/54 ninja-1.13.1.sh
+++ 1.06-Install-System-Software/54 ninja-1.13.1.sh
@@ -1,52 +1,49 @@
-Ninja (1.13.1) - 286 KB:
-Home page: https://ninja-build.org/
-
-Download: https://github.com/ninja-build/ninja/archive/v1.13.1/ninja-1.13.1.tar.gz
-
-MD5 sum: c35f8f55f4cf60f1a916068d8f45a0f8
-
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Ninja (1.13.1) - 286 KB:
+	# Home page: https://ninja-build.org/
+	# Download: https://github.com/ninja-build/ninja/archive/v1.13.1/ninja-1.13.1.tar.gz
+	# MD5 sum: c35f8f55f4cf60f1a916068d8f45a0f8
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Ninja (1.13.1)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://github.com/ninja-build/ninja/archive/v1.13.1/ninja-1.13.1.tar.gz ninja-1.13.1.tar.gz
+	cp ../Packages/ninja-1.13.1.tar.gz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Ninja (1.13.1)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR ninja-1.13.1
+	tar -vxsf ninja-1.13.1.tar.gz
+	cd ninja-1.13.1
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Ninja (1.13.1)..."
 	sleep 2
+	export NINJAJOBS=4
+	sed -i '/int Guess/a \
+  int   j = 0;\
+  char* jobs = getenv( "NINJAJOBS" );\
+  if ( jobs != NULL ) j = atoi( jobs );\
+  if ( j > 0 ) return j;\
+' src/ninja.cc
+	python3 configure.py --bootstrap --verbose
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Installing Ninja (1.13.1)..."
 	sleep 2
-	/bin/bash
-
-	echoL "Installing ------- ( ) ..."
-	sleep 2
+	install -vm755 ninja /usr/bin/
+	install -vDm644 misc/bash-completion /usr/share/bash-completion/completions/ninja
+	install -vDm644 misc/zsh-completion  /usr/share/zsh/site-functions/_ninja
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR ninja-1.13.1
 
 	echoL "Exiting..."
 }
@@ -55,50 +52,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-
-
-
-
-
-
-8.56.1. Installation of Ninja
-When run, ninja normally utilizes the greatest possible number of processes in parallel. By default this is the number of cores on the system, plus two. This may overheat the CPU, or make the system run out of memory. When ninja is invoked from the command line, passing the -jN parameter will limit the number of parallel processes. Some packages embed the execution of ninja, and do not pass the -j parameter on to it.
-
-Using the optional procedure below allows a user to limit the number of parallel processes via an environment variable, NINJAJOBS. For example, setting:
-
-export NINJAJOBS=4
-will limit ninja to four parallel processes.
-
-If desired, make ninja recognize the environment variable NINJAJOBS by running the stream editor:
-
-sed -i '/int Guess/a \
-  int   j = 0;\
-  char* jobs = getenv( "NINJAJOBS" );\
-  if ( jobs != NULL ) j = atoi( jobs );\
-  if ( j > 0 ) return j;\
-' src/ninja.cc
-Build Ninja with:
-
-python3 configure.py --bootstrap --verbose
-The meaning of the build option:
-
---bootstrap
-This parameter forces Ninja to rebuild itself for the current system.
-
---verbose
-This parameter makes configure.py show the progress building Ninja.
-
-The package tests cannot run in the chroot environment. They require cmake. But the basic function of this package is already tested by rebuilding itself (with the --bootstrap option) anyway.
-
-Install the package:
-
-install -vm755 ninja /usr/bin/
-install -vDm644 misc/bash-completion /usr/share/bash-completion/completions/ninja
-install -vDm644 misc/zsh-completion  /usr/share/zsh/site-functions/_ninja
-8.56.2. Contents of Ninja
