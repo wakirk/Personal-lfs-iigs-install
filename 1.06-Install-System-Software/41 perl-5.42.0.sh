be86830d0060f612a70285c417ab4cb42ce20ced
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-18 12:21:05 -0500

    Sync

--- 1.06-Install-System-Software/41 perl-5.42.0.sh
+++ 1.06-Install-System-Software/41 perl-5.42.0.sh
@@ -1,51 +1,61 @@
-Perl (5.42.0) - 14,084 KB:
-Home page: https://www.perl.org/
-
-Download: https://www.cpan.org/src/5.0/perl-5.42.0.tar.xz
-
-MD5 sum: 7a6950a9f12d01eb96a9d2ed2f4e0072
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Perl (5.42.0) - 14,084 KB:
+	# Home page: https://www.perl.org/
+	# Download: https://www.cpan.org/src/5.0/perl-5.42.0.tar.xz
+	# MD5 sum: 7a6950a9f12d01eb96a9d2ed2f4e0072
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Perl (5.42.0)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://www.cpan.org/src/5.0/perl-5.42.0.tar.xz perl-5.42.0.tar.xz
+	cp ../Packages/perl-5.42.0.tar.xz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Perl (5.42.0)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR perl-5.42.0
+	tar -vxsf perl-5.42.0.tar.xz
+	cd perl-5.42.0
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Perl (5.42.0)..."
 	sleep 2
+	export BUILD_ZLIB=False
+	export BUILD_BZIP2=0
+	sh Configure -des                                 \
+		-D prefix=/usr                                \
+		-D vendorprefix=/usr                          \
+		-D privlib=/usr/lib/perl5/5.42/core_perl      \
+		-D archlib=/usr/lib/perl5/5.42/core_perl      \
+		-D sitelib=/usr/lib/perl5/5.42/site_perl      \
+		-D sitearch=/usr/lib/perl5/5.42/site_perl     \
+		-D vendorlib=/usr/lib/perl5/5.42/vendor_perl  \
+		-D vendorarch=/usr/lib/perl5/5.42/vendor_perl \
+		-D man1dir=/usr/share/man/man1                \
+		-D man3dir=/usr/share/man/man3                \
+		-D pager="/usr/bin/less -isR"                 \
+		-D useshrplib                                 \
+		-D usethreads
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Perl (5.42.0)..."
 	sleep 2
-	/bin/bash
+	TEST_JOBS=$(nproc) make test_harness
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Perl (5.42.0)..."
 	sleep 2
+	make install
 
 	echoL "Cleaning up build area...."
 	sleep 2
+	unset BUILD_ZLIB BUILD_BZIP2
 	cd /sources
-	rm -fR 
+	rm -fR perl-5.42.0
 
 	echoL "Exiting..."
 }
@@ -54,51 +64,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-8.43.1. Installation of Perl
-This version of Perl builds the Compress::Raw::Zlib and Compress::Raw::BZip2 modules. By default Perl will use an internal copy of the sources for the build. Issue the following command so that Perl will use the libraries installed on the system:
-
-export BUILD_ZLIB=False
-export BUILD_BZIP2=0
-To have full control over the way Perl is set up, you can remove the “-des” options from the following command and hand-pick the way this package is built. Alternatively, use the command exactly as shown below to use the defaults that Perl auto-detects:
-
-sh Configure -des                                          \
-             -D prefix=/usr                                \
-             -D vendorprefix=/usr                          \
-             -D privlib=/usr/lib/perl5/5.42/core_perl      \
-             -D archlib=/usr/lib/perl5/5.42/core_perl      \
-             -D sitelib=/usr/lib/perl5/5.42/site_perl      \
-             -D sitearch=/usr/lib/perl5/5.42/site_perl     \
-             -D vendorlib=/usr/lib/perl5/5.42/vendor_perl  \
-             -D vendorarch=/usr/lib/perl5/5.42/vendor_perl \
-             -D man1dir=/usr/share/man/man1                \
-             -D man3dir=/usr/share/man/man3                \
-             -D pager="/usr/bin/less -isR"                 \
-             -D useshrplib                                 \
-             -D usethreads
-The meaning of the new Configure options:
-
--D pager="/usr/bin/less -isR"
-This ensures that less is used instead of more.
-
--D man1dir=/usr/share/man/man1 -D man3dir=/usr/share/man/man3
-Since Groff is not installed yet, Configure will not create man pages for Perl. These parameters override this behavior.
-
--D usethreads
-Build Perl with support for threads.
-
-Compile the package:
-
-make
-To test the results, issue:
-
-TEST_JOBS=$(nproc) make test_harness
-Install the package and clean up:
-
-make install
-unset BUILD_ZLIB BUILD_BZIP2
-8.43.2. Contents of Perl
