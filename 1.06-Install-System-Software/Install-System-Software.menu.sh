7fcf8c1d4aeed97343c497f750b332691fdd4f83
wakirk <wakirk2002@gmail.com>
2025-10-18 06:49:40 -0500

    Add bldBison function for Bison installation
    Implement bldBison function to install Bison 3.8.2.

--- 1.06-Install-System-Software/Install-System-Software.menu.sh
+++ 1.06-Install-System-Software/Install-System-Software.menu.sh
@@ -337,7 +337,16 @@ bldGettext() {
 	return 1
 }
 
-bldBison() { :; }
+bldBison() {
+	echoL "Bison (3.8.2)"
+	echoR "Install System Software"
+	chroot_entry
+	chroot_run "$HERE/$EXEC_SCRIPT"
+	chroot_exit
+	return_wait 1
+	return 1
+}
+
 bldGrep() { :; }
 bldBash() { :; }
 bldLibtool() { :; }
@@ -443,7 +452,6 @@ lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
 
-bldBison	na.sh	Undefined.
 bldGrep	na.sh	Undefined.
 bldBash	na.sh	Undefined.
 bldLibtool	na.sh	Undefined.
