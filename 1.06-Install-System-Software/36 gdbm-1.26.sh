bf4205cccde6c8c815ed445b69e037428563d63e
wakirk <wakirk2002@gmail.com>
2025-10-18 10:59:11 -0500

    Remove bldGDBM function from gdbm-1.26.sh
    Removed the bldGDBM function and its related calls.

---
+++ 1.06-Install-System-Software/36 gdbm-1.26.sh
@@ -0,0 +1,74 @@
+GDBM (1.26) - 1,198 KB:
+Home page: https://www.gnu.org/software/gdbm/
+Download: https://ftp.gnu.org/gnu/gdbm/gdbm-1.26.tar.gz
+MD5 sum: aaa600665bc89e2febb3c7bd90679115
+
+/bin/bash
+
+source /root/lfs/lib/menu.lib   # In every script.
+
+main () {
+
+	Utils (5.8.1) - 1,428 KB:
+	Home page: https://tukaani.org/xz
+	Download: https: .tar.xz
+	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	echoR "System Software"
+
+	echoL "Downloading ------- ( ) ..."
+	sleep 2
+	cd "/root/lfs/1.06-Install-System-Software"
+	../bash/Download.sh https://*.tar.xz *.tar.xz
+	cp ../Packages/-.tar.xz /sources
+
+	echoL "Unpack ------- ( ) ..."
+	sleep 2
+	cd /sources
+	rm -fR
+	tar -vxsf
+	cd 
+
+	echoL "Building ------- ( ) ..."
+	sleep 2
+
+	echoL "Installing ------- ( ) ..."
+	sleep 2
+
+	echoL "Cleaning up build area...."
+	sleep 2
+	cd /sources
+	rm -fR 
+
+	echoL "Exiting..."
+}
+
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
+
+exit 1
+
+
+
+
+
+.38.1. Installation of GDBM
+Prepare GDBM for compilation:
+
+./configure --prefix=/usr    \
+            --disable-static \
+            --enable-libgdbm-compat
+The meaning of the configure option:
+
+--enable-libgdbm-compat
+This switch enables building the libgdbm compatibility library. Some packages outside of LFS may require the older DBM routines it provides.
+
+Compile the package:
+
+make
+To test the results, issue:
+
+make check
+Install the package:
+
+make install
+8.38.2. Contents of GDBM
