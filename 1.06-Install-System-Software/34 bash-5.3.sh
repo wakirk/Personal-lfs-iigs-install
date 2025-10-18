7315c6debca90a871a4c004cca9cffef160caeac
wakirk <wakirk2002@gmail.com>
2025-10-18 10:29:20 -0500

    Refactor Bash 5.3 installation script
    Updated the installation script for Bash 5.3 to include downloading, building, and testing steps. Removed outdated sections and added necessary commands for installation and cleanup.

---
+++ 1.06-Install-System-Software/34 bash-5.3.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+source /root/lfs/lib/menu.lib   # In every script.
+
+main () {
+
+	Bash (5.3) - 11,089 KB:
+Home page: https://www.gnu.org/software/bash/
+
+Download: https://ftp.gnu.org/gnu/bash/bash-5.3.tar.gz
+
+MD5 sum: 977c8c0c5ae6309191e7768e28ebc951
+
+echoR "System Software"
+
+	echoL "Downloading Bash (5.3)..."
+	sleep 2
+	cd "/root/lfs/1.06-Install-System-Software"
+	../bash/Download.sh https://*.tar.xz *.tar.xz
+	cp ../Packages/-.tar.xz /sources
+
+	echoL "Unpack Bash (5.3)..."
+	sleep 2
+	cd /sources
+	rm -fR
+	tar -vxsf
+	cd 
+
+	echoL "Building Bash (5.3)..."
+	sleep 2
+./configure --prefix=/usr             \
+            --without-bash-malloc     \
+            --with-installed-readline \
+            --docdir=/usr/share/doc/bash-5.3
+make
+
+	echoL "Testing Bash (5.3)..."
+	sleep 2
+chown -R tester .
+LC_ALL=C.UTF-8 su -s /usr/bin/expect tester << "EOF"
+set timeout -1
+spawn make tests
+expect eof
+lassign [wait] _ _ _ value
+exit $value
+EOF
+
+	echoL "Installing Bash (5.3)..."
+	sleep 2
+make install
+
+	echoL "Cleaning up build area...."
+	sleep 2
+	cd /sources
+	rm -fR 
+
+	echoL "Exiting..."
+}
+
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
+
+exit 1
+
+
+
+
+
+
+
+
+
+
