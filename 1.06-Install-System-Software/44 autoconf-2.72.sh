d139ce0b5c156d86bc74b162aa6b03df21c543fe
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-18 13:07:00 -0500

    Sync

--- 1.06-Install-System-Software/44 autoconf-2.72.sh
+++ 1.06-Install-System-Software/44 autoconf-2.72.sh
@@ -1,53 +1,45 @@
-Autoconf (2.72) - 1,360 KB:
-Home page: https://www.gnu.org/software/autoconf/
-
-Download: https://ftp.gnu.org/gnu/autoconf/autoconf-2.72.tar.xz
-
-MD5 sum: 1be79f7106ab6767f18391c5e22be701
-
-
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Autoconf (2.72) - 1,360 KB:
+	# Home page: https://www.gnu.org/software/autoconf/
+	# Download: https://ftp.gnu.org/gnu/autoconf/autoconf-2.72.tar.xz
+	# MD5 sum: 1be79f7106ab6767f18391c5e22be701
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Autoconf (2.72)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://ftp.gnu.org/gnu/autoconf/autoconf-2.72.tar.xz autoconf-2.72.tar.xz
+	cp ../Packages/autoconf-2.72.tar.xz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Autoconf (2.72)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR autoconf-2.72
+	tar -vxsf autoconf-2.72.tar.xz
+	cd autoconf-2.72
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Autoconf (2.72)..."
 	sleep 2
+	./configure --prefix=/usr
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Autoconf (2.72)..."
 	sleep 2
-	/bin/bash
+	make check
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Autoconf (2.72)..."
 	sleep 2
+	make install
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR autoconf-2.72
 
 	echoL "Exiting..."
 }
@@ -56,26 +48,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-8.46.1. Installation of Autoconf
-Prepare Autoconf for compilation:
-
-./configure --prefix=/usr
-Compile the package:
-
-make
-To test the results, issue:
-
-make check
-Install the package:
-
-make install
-8.46.2. Contents of Autoconf
-
-
-
-
