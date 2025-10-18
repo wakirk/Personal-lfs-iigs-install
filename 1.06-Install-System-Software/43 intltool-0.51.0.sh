d38834f63bd7242f7e67b425a4c7fa9e4e70ecfd
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-18 12:39:09 -0500

    Sync

--- 1.06-Install-System-Software/43 intltool-0.51.0.sh
+++ 1.06-Install-System-Software/43 intltool-0.51.0.sh
@@ -1,52 +1,47 @@
-Intltool (0.51.0) - 159 KB:
-Home page: https://freedesktop.org/wiki/Software/intltool
-
-Download: https://launchpad.net/intltool/trunk/0.51.0/+download/intltool-0.51.0.tar.gz
-
-MD5 sum: 12e517cac2b57a0121cda351570f1e63
-
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Intltool (0.51.0) - 159 KB:
+	# Home page: https://freedesktop.org/wiki/Software/intltool
+	# Download: https://launchpad.net/intltool/trunk/0.51.0/+download/intltool-0.51.0.tar.gz
+	# MD5 sum: 12e517cac2b57a0121cda351570f1e63
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Intltool (0.51.0)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://launchpad.net/intltool/trunk/0.51.0/+download/intltool-0.51.0.tar.gz intltool-0.51.0.tar.gz
+	cp ../Packages/intltool-0.51.0.tar.gz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Intltool (0.51.0)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR intltool-0.51.0
+	tar -vxsf intltool-0.51.0.tar.gz
+	cd intltool-0.51.0
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Intltool (0.51.0)..."
 	sleep 2
+	sed -i 's:\\\${:\\\$\\{:' intltool-update.in
+	./configure --prefix=/usr
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Intltool (0.51.0)..."
 	sleep 2
-	/bin/bash
+	make check
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Intltool (0.51.0)..."
 	sleep 2
+	make install
+	install -v -Dm644 doc/I18N-HOWTO /usr/share/doc/intltool-0.51.0/I18N-HOWTO
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR intltool-0.51.0
 
 	echoL "Exiting..."
 }
@@ -55,30 +50,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-8.45.1. Installation of Intltool
-First fix a warning that is caused by perl-5.22 and later:
-
-sed -i 's:\\\${:\\\$\\{:' intltool-update.in
-[Note] Note
-The above regular expression looks unusual because of all the backslashes. What it does is add a backslash before the right brace character in the sequence '\${' resulting in '\$\{'.
-
-Prepare Intltool for compilation:
-
-./configure --prefix=/usr
-Compile the package:
-
-make
-To test the results, issue:
-
-make check
-Install the package:
-
-make install
-install -v -Dm644 doc/I18N-HOWTO /usr/share/doc/intltool-0.51.0/I18N-HOWTO
-8.45.2. Contents of Intltool
