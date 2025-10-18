598e6812f66e0136c3352cda074b3d494cf9ccaa
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-18 11:40:40 -0500

    Sync

--- 1.06-Install-System-Software/39 inetutils-2.6.sh
+++ 1.06-Install-System-Software/39 inetutils-2.6.sh
@@ -1,48 +1,57 @@
-Inetutils (2.6) - 1,724 KB:
-Home page: https://www.gnu.org/software/inetutils/
-Download: https://ftp.gnu.org/gnu/inetutils/inetutils-2.6.tar.xz
-MD5 sum: 401d7d07682a193960bcdecafd03de94
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Inetutils (2.6) - 1,724 KB:
+	# Home page: https://www.gnu.org/software/inetutils/
+	# Download: https://ftp.gnu.org/gnu/inetutils/inetutils-2.6.tar.xz
+	# MD5 sum: 401d7d07682a193960bcdecafd03de94
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Inetutils (2.6)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://ftp.gnu.org/gnu/inetutils/inetutils-2.6.tar.xz inetutils-2.6.tar.xz
+	cp ../Packages/inetutils-2.6.tar.xz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Inetutils (2.6)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR inetutils-2.6
+	tar -vxsf inetutils-2.6.tar.xz
+	cd inetutils-2.6
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Inetutils (2.6)..."
 	sleep 2
+	sed -i 's/def HAVE_TERMCAP_TGETENT/ 1/' telnet/telnet.c
+	./configure --prefix=/usr \
+		--bindir=/usr/bin     \
+		--localstatedir=/var  \
+		--disable-logger      \
+		--disable-whois       \
+		--disable-rcp         \
+		--disable-rexec       \
+		--disable-rlogin      \
+		--disable-rsh         \
+		--disable-servers
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Inetutils (2.6)..."
 	sleep 2
-	/bin/bash
+	make check
+	echo "One test is known to fail, it's good."
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Inetutils (2.6)..."
 	sleep 2
+	make install
+	mv -v /usr/{,s}bin/ifconfig
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR inetutils-2.6
 
 	echoL "Exiting..."
 }
@@ -51,30 +60,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-sed -i 's/def HAVE_TERMCAP_TGETENT/ 1/' telnet/telnet.c
-./configure --prefix=/usr        \
-            --bindir=/usr/bin    \
-            --localstatedir=/var \
-            --disable-logger     \
-            --disable-whois      \
-            --disable-rcp        \
-            --disable-rexec      \
-            --disable-rlogin     \
-            --disable-rsh        \
-            --disable-servers
-
-make
-make check
-make install
-mv -v /usr/{,s}bin/ifconfig
-
-
-
-
-
