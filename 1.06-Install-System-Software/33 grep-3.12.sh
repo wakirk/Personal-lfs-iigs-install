d54499eed514b84aa76eb0d8dd8308436de2815b
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-18 10:29:39 -0500

    Sync

--- 1.06-Install-System-Software/33 grep-3.12.sh
+++ 1.06-Install-System-Software/33 grep-3.12.sh
@@ -1,25 +1,13 @@
-Grep (3.12) - 1,874 KB:
-Home page: https://www.gnu.org/software/grep/
-
-Download: https://ftp.gnu.org/gnu/grep/grep-3.12.tar.xz
-
-MD5 sum: 5d9301ed9d209c4a88c8d3a6fd08b9ac
-
-
-
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Grep (3.12) - 1,874 KB:
+	# Home page: https://www.gnu.org/software/grep/
+	# Download: https://ftp.gnu.org/gnu/grep/grep-3.12.tar.xz
+	# MD5 sum: 5d9301ed9d209c4a88c8d3a6fd08b9ac
 	echoR "System Software"
 
 	echoL "Downloading Grep (3.12)..."
@@ -37,23 +25,18 @@ main () {
 
 	echoL "Building Grep (3.12)..."
 	sleep 2
-  sed -i "s/echo/#echo/" src/egrep.sh
-  ./configure --prefix=/usr
-  make
+	sed -i "s/echo/#echo/" src/egrep.sh
+	./configure --prefix=/usr
+	make
 
-
-
-
-  echoL "Testing Grep (3.12)..."
+	echoL "Testing Grep (3.12)..."
 	sleep 2
-make check
+	make check
 
-  
 	echoL "Installing Grep (3.12)..."
 	sleep 2
-make install
+	make install
 
-  
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
@@ -66,6 +49,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
