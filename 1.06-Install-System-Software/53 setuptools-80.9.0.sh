ff3a7ee11cd570869be04633e0c5d291889ac05f
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-18 17:36:37 -0500

    Sync

--- 1.06-Install-System-Software/53 setuptools-80.9.0.sh
+++ 1.06-Install-System-Software/53 setuptools-80.9.0.sh
@@ -1,52 +1,40 @@
-Setuptools (80.9.0) - 1,290 KB:
-Home page: https://pypi.org/project/setuptools/
-
-Download: https://pypi.org/packages/source/s/setuptools/setuptools-80.9.0.tar.gz
-
-MD5 sum: 82e1d67883b713f9493659b50d13b436
-
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Setuptools (80.9.0) - 1,290 KB:
+	# Home page: https://pypi.org/project/setuptools/
+	# Download: https://pypi.org/packages/source/s/setuptools/setuptools-80.9.0.tar.gz
+	# MD5 sum: 82e1d67883b713f9493659b50d13b436
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Setuptools (80.9.0)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://pypi.org/packages/source/s/setuptools/setuptools-80.9.0.tar.gz setuptools-80.9.0.tar.gz
+	cp ../Packages/setuptools-80.9.0.tar.gz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Setuptools (80.9.0)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR setuptools-80.9.0
+	tar -vxsf setuptools-80.9.0.tar.gz
+	cd setuptools-80.9.0
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Setuptools (80.9.0)..."
 	sleep 2
+	pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Installing Setuptools (80.9.0)..."
 	sleep 2
-	/bin/bash
-
-	echoL "Installing ------- ( ) ..."
-	sleep 2
+	pip3 install --no-index --find-links dist setuptools
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR setuptools-80.9.0
 
 	echoL "Exiting..."
 }
@@ -55,18 +43,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-8.55.1. Installation of Setuptools
-Build the package:
-
-pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
-Install the package:
-
-pip3 install --no-index --find-links dist setuptools
-8.55.2. Contents of Setuptools
-
-
-
