c953b2b464a7c4a49b3357c58d5fb27a55372158
wakirk <wakirk2002@gmail.com>
2025-10-18 17:08:10 -0500

    Implement bldPackaging function for software installation
    Added packaging function to handle system software installation.

--- 1.06-Install-System-Software/Install-System-Software.menu.sh
+++ 1.06-Install-System-Software/Install-System-Software.menu.sh
@@ -528,6 +528,15 @@ bldFlitCore() {
 }
 
 bldPackaging() { :; }
+	echoL "Packaging (25.0)"
+	echoR "Install System Software"
+	chroot_entry
+	chroot_run "$HERE/$EXEC_SCRIPT"
+	chroot_exit
+	return_wait 1
+	return 1
+}
+
 bldWheel() { :; }
 bldSetuptools() { :; }
 bldNinja() { :; }
@@ -615,7 +624,6 @@ lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
 
-bldPackaging	na.sh	Undefined.
 bldWheel	na.sh	Undefined.
 bldSetuptools	na.sh	Undefined.
 bldNinja	na.sh	Undefined.
