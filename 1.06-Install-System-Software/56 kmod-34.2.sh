10bb5f4ec49450e25df45d124e37af176d47e490
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-18 18:28:34 -0500

    Sync

--- 1.06-Install-System-Software/56 kmod-34.2.sh
+++ 1.06-Install-System-Software/56 kmod-34.2.sh
@@ -1,49 +1,45 @@
-Kmod (34.2) - 434 KB:
-Home page: https://github.com/kmod-project/kmod
-
-Download: https://www.kernel.org/pub/linux/utils/kernel/kmod/kmod-34.2.tar.xz
-
-MD5 sum: 36f2cc483745e81ede3406fa55e1065a
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Kmod (34.2) - 434 KB:
+	# Home page: https://github.com/kmod-project/kmod
+	# Download: https://www.kernel.org/pub/linux/utils/kernel/kmod/kmod-34.2.tar.xz
+	# MD5 sum: 36f2cc483745e81ede3406fa55e1065a
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Kmod (34.2)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://www.kernel.org/pub/linux/utils/kernel/kmod/kmod-34.2.tar.xz kmod-34.2.tar.xz
+	cp ../Packages/kmod-34.2.tar.xz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Kmod (34.2)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR kmod-34.2
+	tar -vxsf kmod-34.2.tar.xz
+	cd kmod-34.2
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Kmod (34.2)..."
 	sleep 2
+	mkdir -p build
+	cd       build
+	meson setup --prefix=/usr ..  \
+		--buildtype=release       \
+		-D manpages=false
+	ninja
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Installing Kmod (34.2)..."
 	sleep 2
-	/bin/bash
-
-	echoL "Installing ------- ( ) ..."
-	sleep 2
+	ninja install
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR kmod-34.2
 
 	echoL "Exiting..."
 }
@@ -52,38 +48,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-
-8.58.1. Installation of Kmod
-Prepare Kmod for compilation:
-
-mkdir -p build
-cd       build
-
-meson setup --prefix=/usr ..    \
-            --buildtype=release \
-            -D manpages=false
-The meaning of the configure options:
-
--D manpages=false
-This option disables generating the man pages which requires an external program.
-
-Compile the package:
-
-ninja
-The test suite of this package requires raw kernel headers (not the “sanitized” kernel headers installed earlier), which are beyond the scope of LFS.
-
-Now install the package:
-
-ninja install
-8.58.2. Contents of Kmod
-
-
-
-
-
