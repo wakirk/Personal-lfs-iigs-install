31803a5fdf1c88d5d619e3648c64060f0e73a661
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-18 11:18:49 -0500

    Sync

--- 1.06-Install-System-Software/37 gperf-3.3.sh
+++ 1.06-Install-System-Software/37 gperf-3.3.sh
@@ -1,45 +1,45 @@
-Gperf (3.3) - 1,789 KB:
-Home page: https://www.gnu.org/software/gperf/
-Download: https://ftp.gnu.org/gnu/gperf/gperf-3.3.tar.gz
-MD5 sum: 31753b021ea78a21f154bf9eecb8b079
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Gperf (3.3) - 1,789 KB:
+	# Home page: https://www.gnu.org/software/gperf/
+	# Download: https://ftp.gnu.org/gnu/gperf/gperf-3.3.tar.gz
+	# MD5 sum: 31753b021ea78a21f154bf9eecb8b079
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Gperf (3.3)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://ftp.gnu.org/gnu/gperf/gperf-3.3.tar.gz gperf-3.3.tar.gz
+	cp ../Packages/gperf-3.3.tar.gz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Gperf (3.3)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR gperf-3.3
+	tar -vxsf gperf-3.3.tar.gz
+	cd gperf-3.3
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Gperf (3.3)..."
 	sleep 2
+	./configure --prefix=/usr --docdir=/usr/share/doc/gperf-3.3
+	make
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Testing Gperf (3.3)..."
 	sleep 2
+	make check
+
+	echoL "Installing Gperf (3.3)..."
+	sleep 2
+	make install
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR gperf-3.3
 
 	echoL "Exiting..."
 }
@@ -48,21 +48,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-8.39.1. Installation of Gperf
-Prepare Gperf for compilation:
-
-./configure --prefix=/usr --docdir=/usr/share/doc/gperf-3.3
-Compile the package:
-
-make
-To test the results, issue:
-
-make check
-Install the package:
-
-make install
-8.39.2. Contents of Gperf
