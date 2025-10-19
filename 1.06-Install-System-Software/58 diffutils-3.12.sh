bd12a0fa101655dd782ba5650aec3dd1a2511386
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-18 19:26:28 -0500

    Sync

--- 1.06-Install-System-Software/58 diffutils-3.12.sh
+++ 1.06-Install-System-Software/58 diffutils-3.12.sh
@@ -1,51 +1,45 @@
-Diffutils (3.12) - 1,894 KB:
-Home page: https://www.gnu.org/software/diffutils/
-
-Download: https://ftp.gnu.org/gnu/diffutils/diffutils-3.12.tar.xz
-
-MD5 sum: d1b18b20868fb561f77861cd90b05de4
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Diffutils (3.12) - 1,894 KB:
+	# Home page: https://www.gnu.org/software/diffutils/
+	# Download: https://ftp.gnu.org/gnu/diffutils/diffutils-3.12.tar.xz
+	# MD5 sum: d1b18b20868fb561f77861cd90b05de4
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Diffutils (3.12)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://ftp.gnu.org/gnu/diffutils/diffutils-3.12.tar.xz diffutils-3.12.tar.xz
+	cp ../Packages/diffutils-3.12.tar.xz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Diffutils (3.12)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR diffutils-3.12
+	tar -vxsf diffutils-3.12.tar.xz
+	cd diffutils-3.12
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Diffutils (3.12)..."
 	sleep 2
+	./configure --prefix=/usr
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Diffutils (3.12)..."
 	sleep 2
-	/bin/bash
+	make check
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Diffutils (3.12)..."
 	sleep 2
+	make install
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR diffutils-3.12
 
 	echoL "Exiting..."
 }
@@ -54,27 +48,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-
-
-
-
-8.60.1. Installation of Diffutils
-Prepare Diffutils for compilation:
-
-./configure --prefix=/usr
-Compile the package:
-
-make
-To test the results, issue:
-
-make check
-Install the package:
-
-make install
-8.60.2. Contents of Diffutils
