3e4ec3c96023c16401df1c5edd6b6ed07e121a9b
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-19 11:22:52 -0500

    Sync

--- 1.06-Install-System-Software/59 gawk-5.3.2.sh
+++ 1.06-Install-System-Software/59 gawk-5.3.2.sh
@@ -1,50 +1,51 @@
-Gawk (5.3.2) - 3,662 KB:
-Home page: https://www.gnu.org/software/gawk/
-Download: https://ftp.gnu.org/gnu/gawk/gawk-5.3.2.tar.xz
-MD5 sum: b7014650c5f45e5d4837c31209dc0037
-
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Gawk (5.3.2) - 3,662 KB:
+	# Home page: https://www.gnu.org/software/gawk/
+	# Download: https://ftp.gnu.org/gnu/gawk/gawk-5.3.2.tar.xz
+	# MD5 sum: b7014650c5f45e5d4837c31209dc0037
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Gawk (5.3.2)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://ftp.gnu.org/gnu/gawk/gawk-5.3.2.tar.xz gawk-5.3.2.tar.xz
+	cp ../Packages/gawk-5.3.2.tar.xz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Gawk (5.3.2)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR gawk-5.3.2
+	tar -vxsf gawk-5.3.2.tar.xz
+	cd gawk-5.3.2
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Gawk (5.3.2)..."
 	sleep 2
+	sed -i 's/extras//' Makefile.in
+	./configure --prefix=/usr
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Gawk (5.3.2)..."
 	sleep 2
+	chown -R tester .
+	su tester -c "PATH=$PATH make check"
 	/bin/bash
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Gawk (5.3.2)..."
 	sleep 2
+	rm -f /usr/bin/gawk-5.3.2
+	make install
+	ln -sv gawk.1 /usr/share/man/man1/awk.1
+	install -vDm644 doc/{awkforai.txt,*.{eps,pdf,jpg}} -t /usr/share/doc/gawk-5.3.2
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR gawk-5.3.2
 
 	echoL "Exiting..."
 }
@@ -53,22 +54,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-
-sed -i 's/extras//' Makefile.in
-./configure --prefix=/usr
-make
-
-chown -R tester .
-su tester -c "PATH=$PATH make check"
-
-Install the package:
-rm -f /usr/bin/gawk-5.3.2
-make install
-ln -sv gawk.1 /usr/share/man/man1/awk.1
-install -vDm644 doc/{awkforai.txt,*.{eps,pdf,jpg}} -t /usr/share/doc/gawk-5.3.2
