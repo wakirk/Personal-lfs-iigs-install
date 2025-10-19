2879a6bc6d2a8caf955f4be16ba9ca5997536080
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-19 14:05:36 -0500

    Sync

--- 1.06-Install-System-Software/61 groff-1.23.0.sh
+++ 1.06-Install-System-Software/61 groff-1.23.0.sh
@@ -1,50 +1,47 @@
-Groff (1.23.0) - 7,259 KB:
-Home page: https://www.gnu.org/software/groff/
-Download: https://ftp.gnu.org/gnu/groff/groff-1.23.0.tar.gz
-MD5 sum: 5e4f40315a22bb8a158748e7d5094c7d
-
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Groff (1.23.0) - 7,259 KB:
+	# Home page: https://www.gnu.org/software/groff/
+	# Download: https://ftp.gnu.org/gnu/groff/groff-1.23.0.tar.gz
+	# MD5 sum: 5e4f40315a22bb8a158748e7d5094c7d
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Groff (1.23.0)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://ftp.gnu.org/gnu/groff/groff-1.23.0.tar.gz groff-1.23.0.tar.gz
+	cp ../Packages/groff-1.23.0.tar.gz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Groff (1.23.0)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR groff-1.23.0
+	tar -vxsf groff-1.23.0.tar.gz
+	cd groff-1.23.0
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Groff (1.23.0)..."
 	sleep 2
+	PAGE=letter ./configure --prefix=/usr
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Groff (1.23.0)..."
 	sleep 2
+	make check
 	/bin/bash
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Groff (1.23.0)..."
 	sleep 2
+	make install
+	echo "letter" > /etc/papersize
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR groff-1.23.0
 
 	echoL "Exiting..."
 }
@@ -53,24 +50,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-.63.1. Installation of Groff
-Groff expects the environment variable PAGE to contain the default paper size. For users in the United States, PAGE=letter is appropriate. Elsewhere, PAGE=A4 may be more suitable. While the default paper size is configured during compilation, it can be overridden later by echoing either “A4” or “letter” to the /etc/papersize file.
-
-Prepare Groff for compilation:
-
-PAGE=<paper_size> ./configure --prefix=/usr
-Build the package:
-
-make
-To test the results, issue:
-
-make check
-Install the package:
-
-make install
-8.63.2. Contents of Groff
