270a312cee90c7034798b39362819fd6ac88bdf6
wakirk <wakirk2002@gmail.com>
2025-10-19 14:05:25 -0500

    Add script for installing GRUB version 2.12

---
+++ 1.06-Install-System-Software/62 grub-2.12.sh
@@ -0,0 +1,9 @@
+8.64. GRUB-2.12      https://www.linuxfromscratch.org/lfs/view/stable/chapter08/grub.html
+
+
+Download (HTTP): https://ftp.gnu.org/gnu/grub/grub-2.12.tar.xz
+Download MD5 sum: 60c564b1bdc39d8e43b3aab4bc0fb140
+Download size: 29 MB
+Estimated disk space required: 174 MB (with optional dependencies and download)
+Estimated build time: 0.6 SBU
+
