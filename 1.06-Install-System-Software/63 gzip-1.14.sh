e5b19ebe10882fe53004162f8067677ac4732f63
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-19 18:43:52 -0500

    Sync

--- 1.06-Install-System-Software/63 gzip-1.14.sh
+++ 1.06-Install-System-Software/63 gzip-1.14.sh
@@ -1,49 +1,45 @@
-Gzip (1.14) - 865 KB:
-Home page: https://www.gnu.org/software/gzip/
-Download: https://ftp.gnu.org/gnu/gzip/gzip-1.14.tar.xz
-MD5 sum: 4bf5a10f287501ee8e8ebe00ef62b2c2
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Gzip (1.14) - 865 KB:
+	# Home page: https://www.gnu.org/software/gzip/
+	# Download: https://ftp.gnu.org/gnu/gzip/gzip-1.14.tar.xz
+	# MD5 sum: 4bf5a10f287501ee8e8ebe00ef62b2c2
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Gzip (1.14)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://ftp.gnu.org/gnu/gzip/gzip-1.14.tar.xz gzip-1.14.tar.xz
+	cp ../Packages/gzip-1.14.tar.xz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Gzip (1.14)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR gzip-1.14
+	tar -vxsf gzip-1.14.tar.xz
+	cd gzip-1.14
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Gzip (1.14)..."
 	sleep 2
+	./configure --prefix=/usr
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Gzip (1.14)..."
 	sleep 2
-	/bin/bash
+	make check
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Gzip (1.14)..."
 	sleep 2
+	make install
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR gzip-1.14
 
 	echoL "Exiting..."
 }
@@ -52,23 +48,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-8.65.1. Installation of Gzip
-Prepare Gzip for compilation:
-
-./configure --prefix=/usr
-Compile the package:
-
-make
-To test the results, issue:
-
-make check
-Install the package:
-
-make install
-8.65.2. Contents of Gzip
-
