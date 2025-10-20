d7e62209e1f597dda80e37b40e0046b0ba616520
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-19 21:07:57 -0500

    Sync

--- 1.06-Install-System-Software/72 markupsafe-3.0.2.sh
+++ 1.06-Install-System-Software/72 markupsafe-3.0.2.sh
@@ -1,56 +1,40 @@
-MarkupSafe (3.0.2) - 21 KB:
-Home page: https://palletsprojects.com/p/markupsafe/
-Download: https://pypi.org/packages/source/M/MarkupSafe/markupsafe-3.0.2.tar.gz
-MD5 sum: cb0071711b573b155cc8f86e1de72167
-
-
-
-
-
-
-
-/bin/bash
-
-
-
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# MarkupSafe (3.0.2) - 21 KB:
+	# Home page: https://palletsprojects.com/p/markupsafe/
+	# Download: https://pypi.org/packages/source/M/MarkupSafe/markupsafe-3.0.2.tar.gz
+	# MD5 sum: cb0071711b573b155cc8f86e1de72167
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading MarkupSafe (3.0.2)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://pypi.org/packages/source/M/MarkupSafe/markupsafe-3.0.2.tar.gz markupsafe-3.0.2.tar.gz
+	cp ../Packages/markupsafe-3.0.2.tar.gz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack MarkupSafe (3.0.2)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR markupsafe-3.0.2
+	tar -vxsf markupsafe-3.0.2.tar.gz
+	cd markupsafe-3.0.2
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building MarkupSafe (3.0.2)..."
 	sleep 2
+	pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Installing MarkupSafe (3.0.2)..."
 	sleep 2
-	/bin/bash
-
-	echoL "Installing ------- ( ) ..."
-	sleep 2
+	pip3 install --no-index --find-links dist Markupsafe
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR markupsafe-3.0.2
 
 	echoL "Exiting..."
 }
@@ -59,22 +43,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-8.74.1. Installation of MarkupSafe
-Compile MarkupSafe with the following command:
-
-pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
-This package does not come with a test suite.
-
-Install the package:
-
-pip3 install --no-index --find-links dist Markupsafe
-8.74.2. Contents of MarkupSafe
-
-
-
