2054a05e7c63b6168668ae9dc0e9dd623d22d656
wakirk <wakirk2002@gmail.com>
2025-10-19 19:16:34 -0500

    Implement bldLibpipeline function for installation

--- 1.06-Install-System-Software/Install-System-Software.menu.sh
+++ 1.06-Install-System-Software/Install-System-Software.menu.sh
@@ -677,7 +677,16 @@ bldKbd() {
 	return 1
 }
 
-bldLibpipeline() { :; }
+bldLibpipeline() {
+	echoL "Libpipeline (1.5.8)"
+	echoR "Install System Software"
+	chroot_entry
+	chroot_run "$HERE/$EXEC_SCRIPT"
+	chroot_exit
+	return_wait 1
+	return 1
+}
+
 bldMake() { :; }
 bldPatch() { :; }
 bldTar() { :; }
@@ -750,7 +759,6 @@ lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
 
-bldLibpipeline	na.sh	Undefined.
 bldMake	na.sh	Undefined.
 bldPatch	na.sh	Undefined.
 bldTar	na.sh	Undefined.
