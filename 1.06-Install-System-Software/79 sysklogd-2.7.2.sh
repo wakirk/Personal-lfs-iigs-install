1375b46250074df97d26ef9073a35151284efe50
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-19 22:43:45 -0500

    Sync

--- 1.06-Install-System-Software/79 sysklogd-2.7.2.sh
+++ 1.06-Install-System-Software/79 sysklogd-2.7.2.sh
@@ -1,89 +1,42 @@
-Sysklogd (2.7.2) - 474 KB:
-Home page: https://www.infodrom.org/projects/sysklogd/
-Download: https://github.com/troglobit/sysklogd/releases/download/v2.7.2/sysklogd-2.7.2.tar.gz
-MD5 sum: af60786956a2dc84054fbf46652e515e
-
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Sysklogd (2.7.2) - 474 KB:
+	# Home page: https://www.infodrom.org/projects/sysklogd/
+	# Download: https://github.com/troglobit/sysklogd/releases/download/v2.7.2/sysklogd-2.7.2.tar.gz
+	# MD5 sum: af60786956a2dc84054fbf46652e515e
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Sysklogd (2.7.2)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://github.com/troglobit/sysklogd/releases/download/v2.7.2/sysklogd-2.7.2.tar.gz sysklogd-2.7.2.tar.gz
+	cp ../Packages/sysklogd-2.7.2.tar.gz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Sysklogd (2.7.2)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR sysklogd-2.7.2
+	tar -vxsf sysklogd-2.7.2.tar.gz
+	cd sysklogd-2.7.2
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Sysklogd (2.7.2)..."
 	sleep 2
+	./configure --prefix=/usr \
+		--sysconfdir=/etc     \
+		--runstatedir=/run    \
+		--without-logger      \
+		--disable-static      \
+		--docdir=/usr/share/doc/sysklogd-2.7.2
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Installing Sysklogd (2.7.2)..."
 	sleep 2
-	/bin/bash
-
-	echoL "Installing ------- ( ) ..."
-	sleep 2
-
-	echoL "Cleaning up build area...."
-	sleep 2
-	cd /sources
-	rm -fR 
-
-	echoL "Exiting..."
-}
-
-lfs_identity
-lfs_tmux_entry main  # must be called after the routine it defines.
-
-exit 1
-
-
-
-
-
-
-
-
-
-
-8.81.1. Installation of Sysklogd
-Prepare the package for compilation:
-
-./configure --prefix=/usr      \
-            --sysconfdir=/etc  \
-            --runstatedir=/run \
-            --without-logger   \
-            --disable-static   \
-            --docdir=/usr/share/doc/sysklogd-2.7.2
-Compile the package:
-
-make
-This package does not come with a test suite.
-
-Install the package:
-
-make install
-8.81.2. Configuring Sysklogd
-Create a new /etc/syslog.conf file by running the following:
-
-cat > /etc/syslog.conf << "EOF"
+	make install
+	cat > /etc/syslog.conf << "EOF"
 # Begin /etc/syslog.conf
 
 auth,authpriv.* -/var/log/auth.log
@@ -99,6 +52,17 @@ secure_mode 2
 
 # End /etc/syslog.conf
 EOF
-8.81.3. Contents of Sysklogd
+	/bin/bash
+	
+	echoL "Cleaning up build area...."
+	sleep 2
+	cd /sources
+	rm -fR sysklogd-2.7.2
 
+	echoL "Exiting..."
+}
 
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
+
+exit 1
