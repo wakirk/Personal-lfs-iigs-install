f6477526967ee6ae61e8418706fc9fff6e941c0c
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-19 22:08:06 -0500

    Sync

--- 1.06-Install-System-Software/76 procps-ng-4.0.5.sh
+++ 1.06-Install-System-Software/76 procps-ng-4.0.5.sh
@@ -1,49 +1,52 @@
-Procps (4.0.5) - 1,483 KB:
-Home page: https://gitlab.com/procps-ng/procps/
-Download: https://sourceforge.net/projects/procps-ng/files/Production/procps-ng-4.0.5.tar.xz
-MD5 sum: 90803e64f51f192f3325d25c3335d057
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Procps (4.0.5) - 1,483 KB:
+	# Home page: https://gitlab.com/procps-ng/procps/
+	# Download: https://sourceforge.net/projects/procps-ng/files/Production/procps-ng-4.0.5.tar.xz
+	# MD5 sum: 90803e64f51f192f3325d25c3335d057
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Procps (4.0.5)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://sourceforge.net/projects/procps-ng/files/Production/procps-ng-4.0.5.tar.xz procps-ng-4.0.5.tar.xz
+	cp ../Packages/procps-ng-4.0.5.tar.xz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Procps (4.0.5)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR procps-ng-4.0.5
+	tar -vxsf procps-ng-4.0.5.tar.xz
+	cd procps-ng-4.0.5
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Procps (4.0.5)..."
 	sleep 2
+	./configure --prefix=/usr                   \
+		--docdir=/usr/share/doc/procps-ng-4.0.5 \
+		--disable-static                        \
+		--disable-kill                          \
+		--enable-watch8bit
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Procps (4.0.5)..."
 	sleep 2
-	/bin/bash
+	chown -R tester .
+	su tester -c "PATH=$PATH make check"
+	echo "One test named ps with output flag bsdtime,cputime,etime,etimes is known to fail if the host kernel"
+	echo "is not built with CONFIG_BSD_PROCESS_ACCT enabled. In addition, one pgrep test may fail in the chroot environment."
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Procps (4.0.5)..."
 	sleep 2
+	make install
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR procps-ng-4.0.5
 
 	echoL "Exiting..."
 }
@@ -52,45 +55,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-
-
-8.78.1. Installation of Procps-ng
-Prepare Procps-ng for compilation:
-
-./configure --prefix=/usr                           \
-            --docdir=/usr/share/doc/procps-ng-4.0.5 \
-            --disable-static                        \
-            --disable-kill                          \
-            --enable-watch8bit
-The meaning of the configure option:
-
---disable-kill
-This switch disables building the kill command; it will be installed from the Util-linux package.
-
---enable-watch8bit
-This switch enables the ncursesw support for the watch command, so it can handle 8-bit characters.
-
-Compile the package:
-
-make
-To run the test suite, run:
-
-chown -R tester .
-su tester -c "PATH=$PATH make check"
-One test named ps with output flag bsdtime,cputime,etime,etimes is known to fail if the host kernel is not built with CONFIG_BSD_PROCESS_ACCT enabled. In addition, one pgrep test may fail in the chroot environment.
-
-Install the package:
-
-make install
-8.78.2. Contents of Procps-ng
-
-
-
-
-
