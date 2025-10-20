b76d4837a7020548eef581da3d397d4cefea7a76
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-19 20:16:44 -0500

    Sync

--- 1.06-Install-System-Software/70 texinfo-7.2.sh
+++ 1.06-Install-System-Software/70 texinfo-7.2.sh
@@ -1,50 +1,53 @@
-Texinfo (7.2) - 6,259 KB:
-Home page: https://www.gnu.org/software/texinfo/
-Download: https://ftp.gnu.org/gnu/texinfo/texinfo-7.2.tar.xz
-MD5 sum: 11939a7624572814912a18e76c8d8972
-
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Texinfo (7.2) - 6,259 KB:
+	# Home page: https://www.gnu.org/software/texinfo/
+	# Download: https://ftp.gnu.org/gnu/texinfo/texinfo-7.2.tar.xz
+	# MD5 sum: 11939a7624572814912a18e76c8d8972
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Texinfo (7.2)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://ftp.gnu.org/gnu/texinfo/texinfo-7.2.tar.xz texinfo-7.2.tar.xz
+	cp ../Packages/texinfo-7.2.tar.xz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Texinfo (7.2)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR texinfo-7.2
+	tar -vxsf texinfo-7.2.tar.xz
+	cd texinfo-7.2
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Texinfo (7.2)..."
 	sleep 2
+	sed 's/! $output_file eq/$output_file ne/' -i tp/Texinfo/Convert/*.pm
+	./configure --prefix=/usr
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Texinfo (7.2)..."
 	sleep 2
-	/bin/bash
+	make check
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Texinfo (7.2)..."
 	sleep 2
+	make install
+	make TEXMF=/usr/share/texmf install-tex
+	pushd /usr/share/info
+	rm -v dir
+	for f in *
+		do install-info $f dir 2>/dev/null
+	done
+	popd
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR texinfo-7.2
 
 	echoL "Exiting..."
 }
@@ -53,52 +56,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-
-8.72.1. Installation of Texinfo
-Fix a code pattern that causes Perl-5.42 or later to display a warning:
-
-sed 's/! $output_file eq/$output_file ne/' -i tp/Texinfo/Convert/*.pm
-Prepare Texinfo for compilation:
-
-./configure --prefix=/usr
-Compile the package:
-
-make
-To test the results, issue:
-
-make check
-Install the package:
-
-make install
-Optionally, install the components belonging in a TeX installation:
-
-make TEXMF=/usr/share/texmf install-tex
-The meaning of the make parameter:
-
-TEXMF=/usr/share/texmf
-The TEXMF makefile variable holds the location of the root of the TeX tree if, for example, a TeX package will be installed later.
-
-The Info documentation system uses a plain text file to hold its list of menu entries. The file is located at /usr/share/info/dir. Unfortunately, due to occasional problems in the Makefiles of various packages, it can sometimes get out of sync with the info pages installed on the system. If the /usr/share/info/dir file ever needs to be recreated, the following optional commands will accomplish the task:
-
-pushd /usr/share/info
-  rm -v dir
-  for f in *
-    do install-info $f dir 2>/dev/null
-  done
-popd
-8.72.2. Contents of Texinfo
-
-
-
-
-
-
-
-
-
