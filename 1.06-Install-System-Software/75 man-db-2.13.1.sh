4be9ff47b79f3a8b9119ae3f3246e9d4e5748f40
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-19 21:57:25 -0500

    Sync

--- 1.06-Install-System-Software/75 man-db-2.13.1.sh
+++ 1.06-Install-System-Software/75 man-db-2.13.1.sh
@@ -1,50 +1,54 @@
-Man-DB (2.13.1) - 2,061 KB:
-Home page: https://www.nongnu.org/man-db/
-Download: https://download.savannah.gnu.org/releases/man-db/man-db-2.13.1.tar.xz
-MD5 sum: b6335533cbeac3b24cd7be31fdee8c83
-
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Man-DB (2.13.1) - 2,061 KB:
+	# Home page: https://www.nongnu.org/man-db/
+	# Download: https://download.savannah.gnu.org/releases/man-db/man-db-2.13.1.tar.xz
+	# MD5 sum: b6335533cbeac3b24cd7be31fdee8c83
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Man-DB (2.13.1)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://download.savannah.gnu.org/releases/man-db/man-db-2.13.1.tar.xz man-db-2.13.1.tar.xz
+	cp ../Packages/man-db-2.13.1.tar.xz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Man-DB (2.13.1)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR man-db-2.13.1
+	tar -vxsf man-db-2.13.1.tar.xz
+	cd man-db-2.13.1
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Man-DB (2.13.1)..."
 	sleep 2
+	./configure --prefix=/usr                 \
+		--docdir=/usr/share/doc/man-db-2.13.1 \
+		--sysconfdir=/etc                     \
+		--disable-setuid                      \
+		--enable-cache-owner=bin              \
+		--with-browser=/usr/bin/lynx          \
+		--with-vgrind=/usr/bin/vgrind         \
+		--with-grap=/usr/bin/grap             \
+		--with-systemdtmpfilesdir=            \
+		--with-systemdsystemunitdir=
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Man-DB (2.13.1)..."
 	sleep 2
-	/bin/bash
+	make check
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Man-DB (2.13.1)..."
 	sleep 2
+	make install
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR man-db-2.13.1
 
 	echoL "Exiting..."
 }
@@ -53,56 +57,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-
-
-
-
-8.77.1. Installation of Man-DB
-Prepare Man-DB for compilation:
-
-./configure --prefix=/usr                         \
-            --docdir=/usr/share/doc/man-db-2.13.1 \
-            --sysconfdir=/etc                     \
-            --disable-setuid                      \
-            --enable-cache-owner=bin              \
-            --with-browser=/usr/bin/lynx          \
-            --with-vgrind=/usr/bin/vgrind         \
-            --with-grap=/usr/bin/grap             \
-            --with-systemdtmpfilesdir=            \
-            --with-systemdsystemunitdir=
-The meaning of the configure options:
-
---disable-setuid
-This disables making the man program setuid to user man.
-
---enable-cache-owner=bin
-This changes ownership of the system-wide cache files to user bin.
-
---with-...
-These three parameters are used to set some default programs. lynx is a text-based web browser (see BLFS for installation instructions), vgrind converts program sources to Groff input, and grap is useful for typesetting graphs in Groff documents. The vgrind and grap programs are not normally needed for viewing manual pages. They are not part of LFS or BLFS, but you should be able to install them yourself after finishing LFS if you wish to do so.
-
---with-systemd...
-These parameters prevent installing unneeded systemd directories and files.
-
-Compile the package:
-
-make
-To test the results, issue:
-
-make check
-Install the package:
-
-make install
-8.77.2. Non-English Manual Pages in LFS
-
-
-
-
-
-
