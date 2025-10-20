8415da4f95107fbdf87cd47650876a5e6849bd78
wakirk <wakirk2002@gmail.com>
2025-10-19 20:16:22 -0500

    Update vim installation script with new commands

---
+++ 1.06-Install-System-Software/71 vim-9.1.1629.sh
@@ -0,0 +1,57 @@
+Vim (9.1.1629) - 18,317 KB:
+Home page: https://www.vim.org
+Download: https://github.com/vim/vim/archive/v9.1.1629/vim-9.1.1629.tar.gz
+MD5 sum: 4f856c3233c1c4570bc17572e4f9e8e4
+
+
+
+/bin/bash
+
+source /root/lfs/lib/menu.lib   # In every script.
+
+main () {
+
+	Utils (5.8.1) - 1,428 KB:
+	Home page: https://tukaani.org/xz
+	Download: https: .tar.xz
+	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	echoR "System Software"
+
+	echoL "Downloading ------- ( ) ..."
+	sleep 2
+	cd "/root/lfs/1.06-Install-System-Software"
+	../bash/Download.sh https://*.tar.xz *.tar.xz
+	cp ../Packages/-.tar.xz /sources
+
+	echoL "Unpack ------- ( ) ..."
+	sleep 2
+	cd /sources
+	rm -fR
+	tar -vxsf
+	cd 
+
+	echoL "Building ------- ( ) ..."
+	sleep 2
+
+	echoL "Testing ------- ( ) ..."
+	sleep 2
+	/bin/bash
+
+	echoL "Installing ------- ( ) ..."
+	sleep 2
+
+	echoL "Cleaning up build area...."
+	sleep 2
+	cd /sources
+	rm -fR 
+
+	echoL "Exiting..."
+}
+
+lfs_identity
+lfs_tmux_entry main  # must be called after the routine it defines.
+
+exit 1
+
+
+
