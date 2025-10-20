7b0be78d836d76e6c3463c2342297e4db93a8d53
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-19 20:02:47 -0500

    Sync

--- 1.06-Install-System-Software/69 tar-1.35.sh
+++ 1.06-Install-System-Software/69 tar-1.35.sh
@@ -1,50 +1,47 @@
-Tar (1.35) - 2,263 KB:
-Home page: https://www.gnu.org/software/tar/
-Download: https://ftp.gnu.org/gnu/tar/tar-1.35.tar.xz
-MD5 sum: a2d8042658cfd8ea939e6d911eaf4152
-
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Tar (1.35) - 2,263 KB:
+	# Home page: https://www.gnu.org/software/tar/
+	# Download: https://ftp.gnu.org/gnu/tar/tar-1.35.tar.xz
+	# MD5 sum: a2d8042658cfd8ea939e6d911eaf4152
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Tar (1.35)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://ftp.gnu.org/gnu/tar/tar-1.35.tar.xz tar-1.35.tar.xz
+	cp ../Packages/tar-1.35.tar.xz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Tar (1.35)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR tar-1.35
+	tar -vxsf tar-1.35.tar.xz
+	cd tar-1.35
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Tar (1.35)..."
 	sleep 2
+	FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix=/usr
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Tar (1.35)..."
 	sleep 2
-	/bin/bash
+	make check
+	echo "One test, capabilities: binary store/restore, is known to fail if it is run because LFS lacks selinux"
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Tar (1.35)..."
 	sleep 2
+	make install
+	make -C doc install-html docdir=/usr/share/doc/tar-1.35
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR tar-1.35
 
 	echoL "Exiting..."
 }
@@ -53,35 +50,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-
-
-
-8.71.1. Installation of Tar
-Prepare Tar for compilation:
-
-FORCE_UNSAFE_CONFIGURE=1  \
-./configure --prefix=/usr
-The meaning of the configure option:
-
-FORCE_UNSAFE_CONFIGURE=1
-This forces the test for mknod to be run as root. It is generally considered dangerous to run this test as the root user, but as it is being run on a system that has only been partially built, overriding it is OK.
-
-Compile the package:
-
-make
-To test the results, issue:
-
-make check
-One test, capabilities: binary store/restore, is known to fail if it is run because LFS lacks selinux, but will be skipped if the host kernel does not support extended attributes or security labels on the filesystem used for building LFS.
-
-Install the package:
-
-make install
-make -C doc install-html docdir=/usr/share/doc/tar-1.35
-8.71.2. Contents of Tar
