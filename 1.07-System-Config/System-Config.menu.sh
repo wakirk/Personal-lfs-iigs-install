0ba1e2f5966c76d29a813cb04243b2631a00dbb5
wakirk <wakirk2002@gmail.com>
2025-10-20 12:36:24 -0500

    Add lfsbootscripts function for LFS boot scripts

--- 1.07-System-Config/System-Config.menu.sh
+++ 1.07-System-Config/System-Config.menu.sh
@@ -46,6 +46,16 @@ postCleanup() {
 	return 1
 }
 
+lfsbootscripts() {
+	echoL "LFS Boot Scripts"
+	echoR "System Configuration"
+	chroot_entry
+	chroot_run "$HERE/$EXEC_SCRIPT"
+	chroot_exit
+	return_wait 1
+	return 1
+}
+
 lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
