16240e67c3d5893c7af153f288d7f6229076ac7e
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-19 19:40:15 -0500

    Sync

--- 1.06-Install-System-Software/67 make-4.4.1.sh
+++ 1.06-Install-System-Software/67 make-4.4.1.sh
@@ -1,49 +1,46 @@
-Make (4.4.1) - 2,300 KB:
-Home page: https://www.gnu.org/software/make/
-Download: https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz
-MD5 sum: c8469a3713cbbe04d955d4ae4be23eeb
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Make (4.4.1) - 2,300 KB:
+	# Home page: https://www.gnu.org/software/make/
+	# Download: https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz
+	# MD5 sum: c8469a3713cbbe04d955d4ae4be23eeb
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Make (4.4.1)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz make-4.4.1.tar.gz
+	cp ../Packages/make-4.4.1.tar.gz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Make (4.4.1)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR make-4.4.1
+	tar -vxsf make-4.4.1.tar.gz
+	cd make-4.4.1
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Make (4.4.1)..."
 	sleep 2
+	./configure --prefix=/usr
+	make
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Testing Make (4.4.1)..."
 	sleep 2
-	/bin/bash
+	chown -R tester .
+	su tester -c "PATH=$PATH make check"
 
-	echoL "Installing ------- ( ) ..."
+	echoL "Installing Make (4.4.1)..."
 	sleep 2
+	make install
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR make-4.4.1
 
 	echoL "Exiting..."
 }
@@ -52,33 +49,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-8.69.1. Installation of Make
-Prepare Make for compilation:
-
-./configure --prefix=/usr
-Compile the package:
-
-make
-To test the results, issue:
-
-chown -R tester .
-su tester -c "PATH=$PATH make check"
-Install the package:
-
-make install
-8.69.2. Contents of Make
