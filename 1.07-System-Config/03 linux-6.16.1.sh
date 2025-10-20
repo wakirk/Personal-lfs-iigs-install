0d18f4ef547524fa10f492c980a433368c7b0c46
wakirk <wakirk2002@gmail.com>
2025-10-20 13:22:17 -0500

    Create 03 linux-6.16.1.sh

---
+++ 1.07-System-Config/03 linux-6.16.1.sh
@@ -0,0 +1,9 @@
+Linux (6.16.1) - 149,042 KB:
+Home page: https://www.kernel.org/
+Download: https://www.kernel.org/pub/linux/kernel/v6.x/linux-6.16.1.tar.xz
+MD5 sum: 32d45755e4b39d06e9be58f6817445ee
+
+
+
+
+
