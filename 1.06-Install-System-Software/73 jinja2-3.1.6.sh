9e50116f670feac0df57e5d52ad2c44769758840
William Kirkpatrick <wakirk2002@gmail.com>
2025-10-19 21:17:33 -0500

    Sync

--- 1.06-Install-System-Software/73 jinja2-3.1.6.sh
+++ 1.06-Install-System-Software/73 jinja2-3.1.6.sh
@@ -1,50 +1,40 @@
-Jinja2 (3.1.6) - 240 KB:
-Home page: https://jinja.palletsprojects.com/en/3.1.x/
-Download: https://pypi.org/packages/source/J/Jinja2/jinja2-3.1.6.tar.gz
-MD5 sum: 66d4c25ff43d1deaf9637ccda523dec8
-
-
-
-
-/bin/bash
+#!/bin/bash
 
 source /root/lfs/lib/menu.lib   # In every script.
 
 main () {
 
-	Utils (5.8.1) - 1,428 KB:
-	Home page: https://tukaani.org/xz
-	Download: https: .tar.xz
-	MD5 sum: cf5e1feb023d22c6bdaa30e84ef3abe3
+	# Jinja2 (3.1.6) - 240 KB:
+	# Home page: https://jinja.palletsprojects.com/en/3.1.x/
+	# Download: https://pypi.org/packages/source/J/Jinja2/jinja2-3.1.6.tar.gz
+	# MD5 sum: 66d4c25ff43d1deaf9637ccda523dec8
 	echoR "System Software"
 
-	echoL "Downloading ------- ( ) ..."
+	echoL "Downloading Jinja2 (3.1.6)..."
 	sleep 2
 	cd "/root/lfs/1.06-Install-System-Software"
-	../bash/Download.sh https://*.tar.xz *.tar.xz
-	cp ../Packages/-.tar.xz /sources
+	../bash/Download.sh https://pypi.org/packages/source/J/Jinja2/jinja2-3.1.6.tar.gz jinja2-3.1.6.tar.gz
+	cp ../Packages/jinja2-3.1.6.tar.gz /sources
 
-	echoL "Unpack ------- ( ) ..."
+	echoL "Unpack Jinja2 (3.1.6)..."
 	sleep 2
 	cd /sources
-	rm -fR
-	tar -vxsf
-	cd 
+	rm -fR jinja2-3.1.6
+	tar -vxsf jinja2-3.1.6.tar.gz
+	cd jinja2-3.1.6
 
-	echoL "Building ------- ( ) ..."
+	echoL "Building Jinja2 (3.1.6)..."
 	sleep 2
+	pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
 
-	echoL "Testing ------- ( ) ..."
+	echoL "Installing Jinja2 (3.1.6)..."
 	sleep 2
-	/bin/bash
-
-	echoL "Installing ------- ( ) ..."
-	sleep 2
+	pip3 install --no-index --find-links dist Jinja2
 
 	echoL "Cleaning up build area...."
 	sleep 2
 	cd /sources
-	rm -fR 
+	rm -fR jinja2-3.1.6
 
 	echoL "Exiting..."
 }
@@ -53,21 +43,3 @@ lfs_identity
 lfs_tmux_entry main  # must be called after the routine it defines.
 
 exit 1
-
-
-
-
-8.75.1. Installation of Jinja2
-Build the package:
-
-pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
-Install the package:
-
-pip3 install --no-index --find-links dist Jinja2
-8.75.2. Contents of Jinja2
-
-
-
-
-
-
