cc92207a9561815e2135b68decdcbe3099991acb
wakirk <wakirk2002@gmail.com>
2025-10-25 01:52:51 -0500

    Create 22 xorg-server.sh

---
+++ 1.08-X-WindowsSystem/22 xorg-server.sh
@@ -0,0 +1,1 @@
+	22. xorg-server	xorg-server — The X display server (“Xorg”) that draws windows on your GPU/monitor. xorg-server (use the built-in modesetting driver; works with kernel DRM)	Start X without a desktop
