d31024392d5fda01500e7c0ba4ac364e4ea56a9f
wakirk <wakirk2002@gmail.com>
2026-02-05 12:43:29 -0600

    Update SetClock.sh to show output for time settings
    Remove redirection of output for timezone and NTP settings.

--- 1.01-Host/01-SetClock.sh
+++ 1.01-Host/01-SetClock.sh
@@ -8,8 +8,8 @@ cd 1.01-Host
 echoR "Configuring Host"
 echoL "Configuring Clock"
 
-timedatectl set-timezone America/Chicago >/dev/null 2>&1 || true
-timedatectl set-ntp true                 >/dev/null 2>&1 || true
+timedatectl set-timezone America/Chicago 
+timedatectl set-ntp true  
 
 exit 1
 
