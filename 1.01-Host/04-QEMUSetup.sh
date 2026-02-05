7cd363c10b00fb1cf87eb265abc01ffac114b1a3
wakirk <wakirk2002@gmail.com>
2026-02-05 12:42:25 -0600

    Fix paths in QEMU setup script
    Updated paths for LFS and sourced menu.lib correctly.

--- 1.01-Host/04-QEMUSetup.sh
+++ 1.01-Host/04-QEMUSetup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source /root/lfs/lib/menu.lib   # In every script.
-cd /root/lfs/1.01-Host
-
+cd /home/lfs/lfs
+source lib/menu.lib
+cd 1.01-Host
 
 echoR "Configuring Host"
 echoL "Installing and Confuring QEMU..."
