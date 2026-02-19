ad07fc92b0b799f60991fa45378ca49f94639f22
wakirk <wakirk2002@gmail.com>
2026-02-19 04:03:07 -0600

    Enhance link-qemu.sh with package installs and mounts
    Install additional packages and set up host share.

--- config/link-qemu.sh
+++ config/link-qemu.sh
@@ -2,5 +2,8 @@
 
 sudo pacman -Syu --needed spice-vdagent
 sudo pacman -Syu --needed plasma-x11-session xorg-server
-
+sudo pacman -Syu --needed tree mc
+mkdir -p /mnt/hostshare
+mount -t 9p -o trans=virtio,version=9p2000.L hostshare /mnt/hostshare
+ln -s /mnt/hostshare ~/share
 exit 0
