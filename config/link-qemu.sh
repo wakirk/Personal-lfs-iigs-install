c872b85402e16a1db2704ddd6f0e99866ada8a6f
wakirk <wakirk2002@gmail.com>
2026-02-21 07:15:02 -0600

    Refactor hostshare mount commands in link-qemu.sh

--- config/link-qemu.sh
+++ config/link-qemu.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
 sudo pacman -Syu --needed spice-vdagent
 sudo pacman -Syu --needed plasma-x11-session xorg-server
 sudo pacman -Syu --needed tree mc
-mkdir -p /mnt/hostshare
-mount -t 9p -o trans=virtio,version=9p2000.L hostshare /mnt/hostshare
-ln -s /mnt/hostshare ~/share
+sudo umount /mnt/hostshare
+sudo rmdir /mnt/hostshare
+sudo mkdir -p /mnt/hostshare
+sudo mount -t 9p -o trans=virtio,version=9p2000.L hostshare /mnt/hostshare
+sudo ln -s /mnt/hostshare ~/share
 exit 0
