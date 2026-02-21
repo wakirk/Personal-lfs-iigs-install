14fcbd88dce16331ec691766f552b089fed0d769
wakirk <wakirk2002@gmail.com>
2026-02-21 07:20:05 -0600

    Update symbolic link path for share directory

--- config/link-qemu.sh
+++ config/link-qemu.sh
@@ -8,5 +8,5 @@ sudo umount /mnt/hostshare
 sudo rmdir /mnt/hostshare
 sudo mkdir -p /mnt/hostshare
 sudo mount -t 9p -o trans=virtio,version=9p2000.L hostshare /mnt/hostshare
-sudo ln -s /mnt/hostshare ~/share
+sudo ln -s /mnt/hostshare /home/wakirk/share
 exit 0
